286c101745b06b55bfb537b3f7fd219f6397b079
Andy R <ar0535@princeton.edu>
2021-11-09 17:14:24 -0500

    Stacks on stacks on stacks

--- bin/GaussSeidel.py
+++ bin/GaussSeidel.py
@@ -7,7 +7,8 @@ class GaussSeidel():
         rhs = np.zeros(4)
         
 
-def rhs_face():
+def rhs_face(params, vnx, vny, sar, dtv2, rmuel, rmuet, rl, pl, ul, vl, rr, pr,
+             ur, vr, a_vec, xx):
     '''
     Calculate the face contributions to the right 
     hand side and preconiditioning matrix
@@ -17,8 +18,100 @@ def rhs_face():
     None.
 
     '''
-    xx = np.zeros((4,4))
+    # Unpack params
+    gamma = params['gamma']
+    kvis  = params['kvis']
+    ncyc  = params['ncyc']
+    prn   = params['prn']
+    prt   = params['prt']
+    diag  = params['diag']
     
+    
+    gm1 = gamma - 1
+    dlim = 0.10
+    if (kvis == 0):
+        dlim = 0.25
+    if (ncyc < 5):
+        dlim = 0.5
+        
+    # Arithmetic averaging
+    rhoa = 0.5*(rl + rr)
+    ua = 0.5*(ul + ur)
+    va = 0.5*(vl + vr)
+    pa = 0.5*(pl + pr)
+    qn = vnx*ua + vny*va
+    cc = gamma*pa/rhoa
+    c = np.sqrt(cc)
+    
+    # Viscous Coefficients
+    epsv1 = 1.0
+    epsv2 = 0.25
+    amu   = epsv1*dtv2*(rmuel + rmuet)
+    amupr = epsv2*dtv2*(rmuel/prn + rmuet/prt)
+    
+    # Eigenvalues
+    e1 = abs(qn)
+    e2 = abs(qn + c)
+    e3 = abs(qn - c)
+    a = dlim*c
+    if (e1 < a):
+        e1 = 0.5*(a + e1**2/a)
+    if (e2 < a):
+        e2 = 0.5*(a + e2**2/a)
+    if (e3 < a):
+        e3 = 0.5*(a + e3**2/a)
+    a = 0.5*sar*dtv2
+    aqsa = a*diag*abs(qn)
+    
+    # Form the right hand side with negative Jacobian
+    q1 = a*(qn - e1)
+    q2 = a*(qn + c - e2)
+    q3 = a*(qn - c - e3)
+    r1 = 0.5*(q2 + q3) - q1
+    r2 = 0.5*(q2 - q3)
+    
+    vq1 = q1 - amu
+    vr1 = r1 - amu/3.0
+    
+    a11 = r1 + q1 - gm1*amupr - aqsa
+    a12 = vnx*r2
+    a13 = vny*r2
+    a14 = -amupr
+    a22 = vnx**2*vr1 + vq1 - aqsa
+    a23 = vnx*vny*vr1
+    a33 = vny**2*vr1 + vq1 - aqsa
+    a41 = -gm1*amupr
+    a44 = q1 - amupr - aqsa
+    
+    rhs = np.zeros(4)
+    a1 = a[0]
+    a2 = a[1]
+    a3 = a[2]
+    a4 = a[3]
+    
+    rhs[0]   = a11*a1  +a12*a2  +a13*a3  +a14*a4
+    rhs[1]   = a12*a1  +a22*a2  +a23*a3
+    rhs[2]   = a13*a1  +a23*a2  +a33*a3
+    rhs[3]   = a41*a1  +a44*a4
+    
+    # Form the preconditioning matrix
+    xx[1,1] -= a11
+    xx[1,2] -= a12
+    xx[1,3] -= a13
+    xx[1,4] -= a14
+    
+    xx[2,1] -= a12
+    xx[2,2] -= a22
+    xx[2,3] -= a23
+    
+    xx[3,1] -= a13
+    xx[3,2] -= a23
+    xx[3,3] -= a33
+    
+    xx[4,1] -= a14
+    xx[4,4] -= a44
+    
+    return xx, rhs
     
 def bcdw(dw, itl, itu):
     '''
