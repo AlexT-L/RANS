03e6e1730098559c5066376e5de3decdb53de7fb
BWynnePU <90227306+BWynnePU@users.noreply.github.com>
2021-11-22 15:08:58 -0500

    Defined all other parameters needed, fixed more spacing for loops, changed incorrect elif to else

--- bin/Viscosity.py
+++ bin/Viscosity.py
@@ -27,11 +27,32 @@ class Viscosity():
 
         il = dims['il']
         jl = dims['jl']
-
+        dim = dims['dim']
+        itl = params['itl']
+        itu = params['itu']
         w = grid.w
 
         # real, dimension(ie,je)            :: u,v,astr,rev0
-        
+
+        # other parameters needed in this 
+        scal = params['scal']
+        chord = params['chord']
+        t0 = params['t0']
+        rmu0 = params['rmu0']
+        p = params['p']
+        mode = params['mode']
+        kturb = params['kturb']
+        rev = params['rev']
+        rev0 = params['rev0']
+        x = params['x']
+        xc = params['xc']
+        u = params['u']
+        v = params['v']
+        ynot = params['ynot']
+        astr = params['astr']
+        rlv = params['rlv']
+        dsti = params['dsti']
+        ib = params['ib']
 
         # useful constants
         pi        = np.pi
@@ -61,51 +82,46 @@ class Viscosity():
         aturb     = 1.
         if (ncyc > 25):
             aturb = .5
-        if (kturb == 1): 
+        if (kturb == 1): # if kturb is one, else  
             for j in range (1,je):
                 for i in range (1,ie):
                     rev0(i,j) = rev(i,j)
-
-
             # call turbbl
             #   to add: 
             # call turb2
             for j in range (1,je):
                 for i in range (1,ie):
                         rev(i,j) = aturb*rev(i,j)  +(1.  -aturb)*rev0(i,j)
-
-                    return
-
         #  else start the rng algebraic model
+        else:
+            for j in range (1,je):
+                for i in range (1,ie):
+                    u(i,j)   = w(i,j,2)/w(i,j,1)
+                    v(i,j)   = w(i,j,3)/w(i,j,1)
 
-        for j in range (1,je):
-            for i in range (1,ie):
-                u(i,j)   = w(i,j,2)/w(i,j,1)
-                v(i,j)   = w(i,j,3)/w(i,j,1)
 
+                for i in range (itl+1,itu):
+                    u(i,1)   = -u(i,2)
+                    v(i,1)   = -v(i,2)
+            for j in range (1,jl):
+                for i in range (1,il):
 
-            for i in range (itl+1,itu):
-                u(i,1)   = -u(i,2)
-                v(i,1)   = -v(i,2)
-        for j in range (1,jl):
-            for i in range (1,il):
-
-                dx13      = xc(i,j,1)   - xc(i+1,j+1,1)
-                dy13      = xc(i,j,2)   - xc(i+1,j+1,2)
-                dx24      = xc(i+1,j,1) - xc(i,j+1,1)
-                dy24      = xc(i+1,j,2) - xc(i,j+1,2)
-                du13      = u(i,j) - u(i+1,j+1)
-                dv13      = v(i,j) - v(i+1,j+1)
-                du24      = u(i+1,j) - u(i,j+1)
-                dv24      = v(i+1,j) - v(i,j+1)
-                ua        = .25*(u(i,j) + u(i+1,j+1) + u(i+1,j) + u(i,j+1))
-                va        = .25*(v(i,j) + v(i+1,j+1) + v(i+1,j) + v(i,j+1))
-                dsij      = 1./(dx13*dy24 - dx24*dy13)
-                dvdx      =  dsij * (dv13*dy24 - dv24*dy13)
-                dudy      = -dsij * (du13*dx24 - du24*dx13)
-                dudx      =  dsij * (du13*dy24 - du24*dy13)
-                dvdy      = -dsij * (dv13*dx24 - dv24*dx13)
-                astr(i,j) = (dudy+dvdx)**2. +2.*(dudx**2  +dvdy**2  -((dudx+dvdy)**2)/3.)
+                    dx13      = xc(i,j,1)   - xc(i+1,j+1,1)
+                    dy13      = xc(i,j,2)   - xc(i+1,j+1,2)
+                    dx24      = xc(i+1,j,1) - xc(i,j+1,1)
+                    dy24      = xc(i+1,j,2) - xc(i,j+1,2)
+                    du13      = u(i,j) - u(i+1,j+1)
+                    dv13      = v(i,j) - v(i+1,j+1)
+                    du24      = u(i+1,j) - u(i,j+1)
+                    dv24      = v(i+1,j) - v(i,j+1)
+                    ua        = .25*(u(i,j) + u(i+1,j+1) + u(i+1,j) + u(i,j+1))
+                    va        = .25*(v(i,j) + v(i+1,j+1) + v(i+1,j) + v(i,j+1))
+                    dsij      = 1./(dx13*dy24 - dx24*dy13)
+                    dvdx      =  dsij * (dv13*dy24 - dv24*dy13)
+                    dudy      = -dsij * (du13*dx24 - du24*dx13)
+                    dudx      =  dsij * (du13*dy24 - du24*dy13)
+                    dvdy      = -dsij * (dv13*dx24 - dv24*dx13)
+                    astr(i,j) = (dudy+dvdx)**2. +2.*(dudx**2  +dvdy**2  -((dudx+dvdy)**2)/3.)
 
 
             #   to add: 
@@ -118,18 +134,16 @@ class Viscosity():
             # also they did not have a corresponding end do?
         for j in range (2,jl):
             for i in range (2,il):
-
-            xbi       = .5*(x(i-1,1,1)  +x(i,1,1))
-            ybi       = .5*(x(i-1,1,2)  +x(i,1,2))
-            astra     = .25*(astr(i-1,j-1)  +astr(i-1,j)
-            .                +astr(i,j-1)    +astr(i,j))
-            if (i>=itl) and (i<=itu+1):
-                a3        = 1./(.225*abs(ynot(i)))
-                ysci      = np.sqrt((xc(i,j,1)  -xbi)**2  +(xc(i,j,2)  -ybi)**2)
-                ysc       = w(i,2,1)/(ysci*w(i,j,1))
-                csc       = 1./(ysc+a3)**2
-            elif:
-                csc       = (cwk*ynot(i))**2
+                xbi       = .5*(x(i-1,1,1)  +x(i,1,1))
+                ybi       = .5*(x(i-1,1,2)  +x(i,1,2))
+                astra     = .25*(astr(i-1,j-1)  +astr(i-1,j)+astr(i,j-1)    +astr(i,j))
+                if (i>=itl) and (i<=itu+1):
+                    a3        = 1./(.225*abs(ynot(i)))
+                    ysci      = np.sqrt((xc(i,j,1)  -xbi)**2  +(xc(i,j,2)  -ybi)**2)
+                    ysc       = w(i,2,1)/(ysci*w(i,j,1))
+                    csc       = 1./(ysc+a3)**2
+                else:
+                    csc       = (cwk*ynot(i))**2
 
 
         #     set some parameters
@@ -144,10 +158,10 @@ class Viscosity():
 
         #     solve for the eddy viscosity
 
-            if (dim(rnut0*a1,a2).eq.0.):
+            if (dim(rnut0*a1,a2) == 0.):
                 rev(i,j)  = 0.
                 #  go to 20? 
-            elif:
+            else:
                 rnut      = np.sqrt(a1)
             
 
@@ -159,19 +173,20 @@ class Viscosity():
         den    = 1./(4.*rnut*rnut*rnut + fac)
         rnut1  = rnut - (rnut**4+rnut*fac  -rnut0*rnut0*a1)*den
 
-            if (abs((rnut1  -rnut))<=1.e-3):
-                rev(i,j) = w(i,j,1)*dim(rnut1,rnul)
-                # go to 20
-            elif
-                k      = k  +1
-                if (k>200):
-                    write (6,*) ' iteration not converged ',i,j
-                    write (6,*) ' rnut = ',rnut,' rnut1 =',rnut1
-                    rev(i,j)  = w(i,j,1)*dim(rnut1,rnul)
-                    # go to 20
+        if (abs((rnut1  -rnut))<=1.e-3):
+            rev(i,j) = w(i,j,1)*dim(rnut1,rnul)
+            # go to 20
+        else:
+            k      = k  +1
+            if (k>200):
+                print(*[' iteration not converged ',i,j])
+                print(*[' rnut = ',rnut,' rnut1 =',rnut1])
 
-                rnut   = rnut1
-                # go to 11
+                rev(i,j)  = w(i,j,1)*dim(rnut1,rnul)
+                # go to 20
+
+            rnut   = rnut1
+            # go to 11, which is above this loop, so repeat until that condition is no longer satisfied?
 
         # those would 'go' here, and then just continue?
         #    20 continue
@@ -185,9 +200,9 @@ class Viscosity():
             ii        = ii  -1
             for j in range (2,jl):
                 pex       = -(xc(i,2,1)  -xc(itl+1,2,1))/(20.*dsti(itl+1))
-                rev(i,j)  = rev(i,j)  +(rev(itl+1,j)  -rev(i,j))*exp(pex)
+                rev(i,j)  = rev(i,j)  +(rev(itl+1,j)  -rev(i,j))*np.exp(pex)
                 pex       = -(xc(ii,2,1)  -xc(itu,2,1))/(20.*dsti(itu))
-                rev(ii,j) = rev(ii,j)  +(rev(itu,j)  -rev(ii,j))*exp(pex)
+                rev(ii,j) = rev(ii,j)  +(rev(itu,j)  -rev(ii,j))*np.exp(pex)
 
         for i in range (2,il):
                 ii        = ib  -i
@@ -197,7 +212,7 @@ class Viscosity():
         for i in range (itl+1,itu):
                 if (xc(i,2,1).le.xtran):
                     for j in range (1,jl):
-                    rev(i,j)  = 0
+                        rev(i,j)  = 0
                 rev(i,1)  = -rev(i,2)
 
         for j in range (1,je):
