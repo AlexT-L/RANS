3d2e4ceb4c6f4baaedc42a634236941754e44dd0
andybroth <ar0535@princeton.edu>
2021-12-06 19:38:35 -0500

    Added empty functions

--- bin/Workspace.py
+++ bin/Workspace.py
@@ -10,3 +10,12 @@ class Workspace:
     def add_field(self, new_field):
         # Adds a new field to fields? 
         return 0
+    
+    def move_up(stencil, field):
+        return 0
+    
+    def move_down(stencil, field):
+        return 0
+    
+    def move_to(stencil, field):
+        return 0
