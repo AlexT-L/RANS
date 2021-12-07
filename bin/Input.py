136df7b6cf249170947be6c37c12276d2305027b
Vedin Dewan <vedindewan@dynamic-oit-vapornet100-10-9-112-240.princeton.edu>
2021-12-07 17:59:32 -0500

    updated max_cols and read function

--- bin/Input.py
+++ bin/Input.py
@@ -1,23 +1,26 @@
 import numpy as np 
 import pandas as pd
 
-### Loop the data lines
-with open("rae9-s1.data", 'r') as temp_f:
-    # get No of columns in each line
-    col_count = [ len(l.split()) for l in temp_f.readlines() ]
-    #^no argment for split(), splits based on white space
+###Get max number of columns in a row
+def max_no_cols(file):
+    ### Loop the data lines
+    with open(file, 'r') as temp_f:
+        # get No of columns in each line
+        col_count = [ len(l.split()) for l in temp_f.readlines() ]
+        #^no argment for split(), splits based on white space
+    return max(col_count)
 
-### Generate column names  (names will be 0, 1, 2, ..., maximum columns - 1)
-column_names = range(0, max(col_count)) 
-#^tells pandas what the max number of columns is 
 
-### Read csv
-df = pd.read_csv("rae9-s1.data", header=None, delimiter="\s+|;|:", names=column_names,engine="python")
-cs=df.iloc[8]
-print(cs)
+### Read .data file
 
-class Input:
+def read(file,max_cols):
+    df = pd.read_csv(file, header=None, delimiter="\s+|;|:", names=range(max_cols),engine="python")
+    return df
+
+#class Input:
     
     # Constructor
-    def __init__(self, filename):
-        pass
+    #def __init__(self, filename):
+     #   pass
+max=max_no_cols("rae9-s1.data")
+print (read("rae9-s1.data",max))
