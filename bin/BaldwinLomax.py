f5ba68b841716826cfa319d9d422e12b45fbfa3c
BWynnePU <90227306+BWynnePU@users.noreply.github.com>
2021-12-07 15:42:58 -0500

    Started converting to python formatting, converting and removing comments

--- bin/BaldwinLomax.py
+++ bin/BaldwinLomax.py
@@ -1,43 +1,22 @@
-      subroutine  turb2
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *   baldwin-lomax turbulence model:  modtur = 2                      *
-c     *                                                                    *
-c     *   calculates turbulent viscosity at the cell faces and then        *
-c     *   averages to obtain cell center values                            *
-c     *   fully vectorized routine                                         *
-c     *                                                                    *
-c     **********************************************************************
-c
-      use dims
-c
-c     ******************************************************************
-c
-      use flo_var
-c
-c     ******************************************************************
-c
-      use solv_var
-c
-c     ******************************************************************
-c
-      use mesh_var
-c
-c     ******************************************************************
-c
-      use psm_var
-c
-c     ******************************************************************
-c
-      use flo_param
-c
-c     ******************************************************************
-c
-      use solv_param
-c
-c     ******************************************************************
-c
+import numpy as np
+from Grid import Grid
+
+class BaldwinLomax():
+    def turbulent_viscosity(grid: Grid, params, dims):
+      # from subroutine turb2.f
+
+    #  **********************************************************************
+    #  *   baldwin-lomax turbulence model:  modtur = 2                      *
+    #  *                                                                    *
+    #  *   calculates turbulent viscosity at the cell faces and then        *
+    #  *   averages to obtain cell center values                            *
+    #  *   fully vectorized routine                                         *
+    #  **********************************************************************
+
+    # "uses:" 
+    # dims, flo_var, solv_var, mesh_var, psm_var, flo_param, solv_param
+
+
       dimension       uedge(il),tauw(il),yscal(il),scalf(il),avor(il),
      .                avorm(il),ravg(il),amuto(il),amuti(il),yvor(il),
      .                yvorm(il),utotm(il),ylenm(il),fkleb(il),jedge(il)
@@ -47,43 +26,41 @@ c
      .                t(ib,jb),ylen(ib,jb)
       dimension       amut(ie,je)
       dimension       test(10)
-c
+
       i2        = ie
       j2        = je
-co    il        = i2- 1
-co    jl        = j2- 1
+    il        = i2- 1
+    jl        = j2- 1
       jlm       = jl- 1
-c
+
       jstop     = 3* (j2- 2)/5
-co    if (cmesh .lt. 0.0) jstop = jl- 1
+    if (cmesh .lt. 0.0) jstop = jl- 1
       itlp      = itl+ 1
       iwrit     = 6
-c
+
       aplusi    = 1./26.
       cwk1      = 1.0
       ckleb     = 0.3
       ccp       = 1.6
       modbl     = 3
       restarr   = 0
-c
+
       rey       = re
       rein      = 1.0/rey
       sgam      = sqrt(gamma)
       sgrm      = sgam*rm
       sgrmi     = 1.0/sgrm
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *    next 2 lines of code is activated to "test" turbulence model    *
-c     *      effect on convergence                                         *
-c     *                                                                    *
-c     *    turbulent viscosity is frozen after ncyct cycles                *
-c     *                                                                    *
-c     **********************************************************************
-c
-c     ncyct     = 10
-c     if (ncyc .gt. ncyct) return
-c
+
+# c     **********************************************************************
+# c     *    next 2 lines of code is activated to "test" turbulence model    *
+# c     *      effect on convergence                                         *
+# c     *                                                                    *
+# c     *    turbulent viscosity is frozen after ncyct cycles                *
+# c     **********************************************************************
+
+     ncyct     = 10
+     if (ncyc .gt. ncyct) return
+
       do 5 j=1,j2
       do 5 i=1,i2
         rinv(i,j) = 1.0/w(i,j,1)
@@ -93,24 +70,22 @@ c
         amu(i,j)  = t(i,j)
         amut(i,j) = 0.0
     5 continue
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *   determination of eddy viscosity                                  *
-c     *                                                                    *
-c     *   turbulence model:                                                *
-c     *     wall boundary layer --- baldwin-lomax model                    *
-c     *     wake region         --- baldwin-lomax model (cwake= 1.0)       *
-c     *                                                                    *
-c     *   calculate vorticity and total velocity                           *
-c     *                                                                    *
-c     **********************************************************************
-c
+
+# c     **********************************************************************
+# c     *   determination of eddy viscosity                                  *
+# c     *                                                                    *
+# c     *   turbulence model:                                                *
+# c     *     wall boundary layer --- baldwin-lomax model                    *
+# c     *     wake region         --- baldwin-lomax model (cwake= 1.0)       *
+# c     *                                                                    *
+# c     *   calculate vorticity and total velocity                           *
+# c     **********************************************************************
+
       do 10 j=1,jl
       do 10 i=1,i2
         vola(i,j) = 0.5* (vol(i,j)+ vol(i,j+1))
    10 continue
-c
+
       do 15 i=2,il
         xxa       = x(i,1,1)-x(i-1,1,1)
         yxa       = x(i,1,2)-x(i-1,1,2)
@@ -126,7 +101,7 @@ c
         utotal    = uavg*uavg + vavg*vavg
         utot(i,1) = sqrt(utotal)
    15 continue
-c
+
       do 20 j=2,jlm
       do 20 i=2,il
         xxa       = x(i,j,1)-x(i-1,j,1)
@@ -135,13 +110,13 @@ c
         vy        = v(i,j+1)- v(i,j)
         uavg      = 0.5* (u(i,j)+ u(i,j+1))
         vavg      = 0.5* (v(i,j)+ v(i,j+1))
-c
-c     thin-layer navier-stokes contribution to vorticity
-c
+
+    #  thin-layer navier-stokes contribution to vorticity
+
         vor1      = (xxa*uy + yxa*vy)/vola(i,j)
-c
-c     additional contributions to vorticity
-c
+
+    #  additional contributions to vorticity
+
         xyw       = 0.5* (x(i-1,j+1,1)- x(i-1,j-1,1))
         xye       = 0.5* (x(i,j+1,1)- x(i,j-1,1))
         yyw       = 0.5* (x(i-1,j+1,2)- x(i-1,j-1,2))
@@ -159,9 +134,9 @@ c
         utotal    = uavg* uavg+ vavg* vavg
         utot(i,j) = sqrt(utotal)
    20 continue
-c
-c     determine transition index
-c
+
+    #  determine transition index
+
       itr1      = 0
       itr2      = 0
       j         = 1
@@ -172,7 +147,7 @@ c
         end if
       end do
    21 continue
-c
+
       itr1p     = itr1 + 1
       do i=itr1p,il
         if (x(i,j,1) .ge. xtran) then
@@ -181,33 +156,32 @@ c
         end if
       end do
    22 continue
-c
+
       do 30 i=2,il
-c
+
         do 25 j=1,jlm
           avor(j)   = vor(i,j)
           utot1(j)  = utot(i,j)
    25   continue
-c
-c
-c     effect of using jlm or jstop needs to be checked;
-c     it does not seem to make a difference
+
+# c     effect of using jlm or jstop needs to be checked;
+# c     it does not seem to make a difference
 c
 c       jmaxv     = ismax(jstop,avor,1)
         jmaxv     = ismax(jlm,avor,1)
         if (jmaxv .eq. 0) jmaxv = 1
-cc
+
         jminut    = ismin(jlm,utot1,1)
-cc
+
         jmaxut    = ismax(jstop,utot1,1)
-cc
+
         avorm(i)  = avor(jmaxv)
         utmin(i)  = utot1(jminut)
         utmax(i)  = max(utot1(jmaxut),1.e-3)
         utotm(i)  = utmax(i)
         yscal(i)  = 1000000.
    30 continue
-c
+
       if (modbl .eq. 1) then
         tur1    = 1.0
         tur2    = 0.
@@ -221,7 +195,7 @@ c
         tur2    = 0.
         tur3    = 1.0
       end if
-c
+
       do 35 i=itlp,itu
         xxa       = x(i,1,1)- x(i-1,1,1)
         yxa       = x(i,1,2)- x(i-1,1,2)
@@ -236,15 +210,13 @@ c
         avor2     = tur1*avora + tur2*avorb + tur3*avorc
         yscal(i)  = sqrt(rey* sgrmi* amub* avor2* w(i,2,1))/(26.*amub)
    35 continue
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *   compute normal distance ylen(i,j) and function  yvor             *
-c     *   (yvor = y* vorticity)                                            *
-c     *                                                                    *
-c     **********************************************************************
-c
-co    if (ncyc .eq. ncyci1) then
+
+        # **********************************************************************
+        # *   compute normal distance ylen(i,j) and function  yvor             *
+        # *   (yvor = y* vorticity)                                            *
+        # **********************************************************************
+
+    if (ncyc .eq. ncyci1) then
         do 39 i=2,il
           ylen(i,1) = 0.0
           do 37 j=2,jl
@@ -258,8 +230,8 @@ co    if (ncyc .eq. ncyci1) then
             ylen(i,j) = ylen(i,j-1)+ scalf(j)
    37     continue
    39   continue
-co    end if
-c
+    end if
+
       do 50 i=2,il
         ylen1     = 0.5* ylen(i,2)
         do 40 j=1,jstop
@@ -267,25 +239,25 @@ c
           damp      = 1.0- exp(-y1)
           yvor(j)   = ylen(i,j)* vor(i,j)* damp
    40   continue
-c
+
         jmaxyv    = ismax(jstop,yvor,1)
         jmaxyv    = max(jmaxyv,2)
         jedge(i)  = jmaxyv
-c
-c     next line of code replaced because it caused convergence
-c     stall when m = 0.001 - 12/10/05 (check this further !!!!)
-c
+
+        # next line of code replaced because it caused convergence
+        # stall when m = 0.001 - 12/10/05 (check this further !!!!)
+
         yvorm(i)  = max(yvor(jmaxyv),1.e-6)
         ylenm(i)  = max(ylen(i,jmaxyv),ylen1)
-c
+
         if (jedge(i) .lt. jstop) then
           ylenm1  = ylenm(i)
-c
+
           if (ncyc.ge.10 .or. restarr.eq.1.0) then
             jmyv    = jedge(i)
             dyvm    = yvor(jmyv)-yvor(jmyv-1)
             dyvp    = yvor(jmyv)-yvor(jmyv+1)
-c
+
             if (yvor(jmyv-1) .lt. yvor(jmyv+1)) then
               ylenm(i) = ylen(i,jmyv)+ .5*(ylen(i,jmyv+1)- ylen(i,jmyv))
      .                   *(1.- dyvp/dyvm)
@@ -293,24 +265,22 @@ c
               ylenm(i) = ylen(i,jmyv)- .5*(ylen(i,jmyv)- ylen(i,jmyv-1))
      .                   *(1.- dyvm/dyvp)
             end if
-c
+
           else
             ylenm(i)  = ylenm1
           end if
-c
+
         end if
    50 continue
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *   compute outer eddy viscosity                                     *
-c     *                                                                    *
-c     *   outer do loop                                                    *
-c     *                                                                    *
-c     **********************************************************************
-c
+
+        # **********************************************************************
+        # *   compute outer eddy viscosity                                     *
+        # *                                                                    *
+        # *   outer do loop                                                    *
+        # **********************************************************************
+
       do 200 i=2,il
-c
+
         udiff     = abs(utmax(i)- utmin(i))
         udiff1    = cwk1* udiff
         do 60 j=2,jstop
@@ -327,13 +297,11 @@ c
           amuto(j)  = abs(amuto(j))
    60   continue
         amuto(1)  = amuto(2)
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *   compute inner eddy viscosity                                     *
-c     *                                                                    *
-c     **********************************************************************
-c
+
+        # **********************************************************************
+        # *   compute inner eddy viscosity                                     *
+        # **********************************************************************
+
         do 70 j=2,jstop
           y1        = yscal(i)* ylen(i,j)
           damp      = 1.0- exp(-y1)
@@ -344,11 +312,11 @@ c
    70   continue
         amuti(1)  = 0.0
         if (i.le.itl .or. i.gt.itu) amuti(1) = amuti(2)
-c
-c        load viscosity coeffs. into array, use inner value until
-c        match point is reached
-c        scalar coding
-c
+
+        # load viscosity coeffs. into array, use inner value until
+        # match point is reached
+        # scalar coding
+
         ivect     = 1
         if (ivect .eq. 0) then
           icross    = 0
@@ -381,34 +349,30 @@ c
             amut(i,j) = amuto(j)
   100     continue
         end if
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *   compute turbulent viscosity at cell center                       *
-c     *                                                                    *
-c     **********************************************************************
-c
+
+        # **********************************************************************
+        # *   compute turbulent viscosity at cell center                       *
+        # **********************************************************************
+
         do 110 j=2,jstop
           amutc     = 0.5* (amut(i,j)+ amut(i,j-1))
           amu(i,j)  = amutc
   110   continue
-c
+
         do 120 j=2,jstop
           amut(i,j) = amu(i,j)
   120   continue
-c
+
         if (i.gt.itr1 .and. i.le.itr2) then
           do 130 j=2,jstop
             amut(i,j) = 0.
   130     continue
         end if
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *   debugging check (activate with jwrit = 1)                        *
-c     *                                                                    *
-c     **********************************************************************
-c
+
+# **********************************************************************
+# *   debugging check (activate with jwrit = 1)                        *
+# **********************************************************************
+
         jwrit     = 0
         if (jwrit .eq. 1) then
           write(6,8000) i
@@ -423,12 +387,12 @@ c
             write(6,8100) j,y1,fkleb(j),ravg(j),cmuti,cmuto,amut(i,j)
   195     continue
         end if
-c
-c       finish of outer loop
+
+       # finish of outer loop
   200 continue
-c
-c       copy amut to rev
-c
+
+       # copy amut to rev
+
       scale     = 1.
       do j=1,je
       do i=1,ie
@@ -436,24 +400,22 @@ c
       end do
       end do
       return
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *   printout for boundary layer details                              *
-c     *   nturbw  -  input for frequency of printing b.l. data             *
-c     *              (currently set at large value)                        *
-c     *                                                                    *
-c     **********************************************************************
-c
+
+    # **********************************************************************
+    # *   printout for boundary layer details                              *
+    # *   nturbw  -  input for frequency of printing b.l. data             *
+    # *              (currently set at large value)                        *
+    # **********************************************************************
+
       nturbw = 5000
-c
+
       if (mod(ncyc,nturbw) .eq. 0) then
         do 250 i=2,il
           if(i .eq. 2) write(iwrit,840)
           write (iwrit,850) i,jedge(i),ylenm(i),utotm(i),yvorm(i),
      .                      yscal(i)
   250   continue
-c
+
         do 300 i=itlp,itu
           if(i.eq.itlp) write(6,860)
           je      = jedge(i)
@@ -468,7 +430,7 @@ c
           cf1     = 2.0* rein* sgrmi* tauw1
           write(6,870) i,je,ue1,delta1,dstar1,tauw1,cf1,yplus
   300   continue
-c
+
         iloc    = itu
         atauw   = abs(tauw(iloc))
         atauwr  = atauw* rinv(iloc,2)
@@ -486,14 +448,12 @@ c
      .                 yscal(iloc),yplus
   310   continue
       end if
-c
-c
-c     **********************************************************************
-c     *                                                                    *
-c     *   additional checks of routine (activate with kwrite = 1)          *
-c     *                                                                    *
-c     **********************************************************************
-c
+
+
+        # **********************************************************************
+        # *   additional checks of routine (activate with kwrite = 1)          *
+        # **********************************************************************
+
       kwrite1   = 0
       if (kwrite1.eq.1 .and. ncyc.eq.mcyc) then
         do i=itl,itl-4,-4
@@ -502,19 +462,19 @@ c
   971     format (5x,'i,j,amut = ',2i5,e15.6)
         end do
         end do
-c
+
         itup      = itu + 1
         do i=itup,itup+4,4
         do j=1,10
           write(970,971) i,j,amut(i,j)
         end do
         end do
-c
-c---->    finish of kwrite if
+
+# ---->    finish of kwrite if
         end if
-c
+
       return
-c
+
   800 format(1h ,'ylen,ravg,amuto1,amuto2,amuto,udiff,fkleb')
   810 format(1h ,5x,i5,7e15.6)
   820 format(1h ,'ylen,utot,vor,yvor,amuti,amuto,fkleb')
