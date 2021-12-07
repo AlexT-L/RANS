dd7135a219b46f6bd2d6d0051ed362f9d993d8cf
Vedin Dewan <vedindewan@dynamic-oit-vapornet100-10-9-112-240.princeton.edu>
2021-12-07 17:23:31 -0500

    made basic read function

--- bin/Input.py
+++ bin/Input.py
@@ -1,4 +1,20 @@
+import numpy as np 
+import pandas as pd
 
+### Loop the data lines
+with open("rae9-s1.data", 'r') as temp_f:
+    # get No of columns in each line
+    col_count = [ len(l.split()) for l in temp_f.readlines() ]
+    #^no argment for split(), splits based on white space
+
+### Generate column names  (names will be 0, 1, 2, ..., maximum columns - 1)
+column_names = range(0, max(col_count)) 
+#^tells pandas what the max number of columns is 
+
+### Read csv
+df = pd.read_csv("rae9-s1.data", header=None, delimiter="\s+|;|:", names=column_names,engine="python")
+cs=df.iloc[8]
+print(cs)
 
 class Input:
     
