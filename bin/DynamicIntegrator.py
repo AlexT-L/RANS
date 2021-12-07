2e7207b09a3a5105df0785a6f44fb25b5a4e2357
andybroth <ar0535@princeton.edu>
2021-12-06 19:35:13 -0500

    Wrote super basic outline

---
+++ bin/DynamicIntegrator.py
@@ -0,0 +1,13 @@
+
+
+class DynamicIntegrator:
+    
+    # Constructor
+    def __init__(self, nStages, weights):
+        self.nStages = nStages
+        self.weights = weights
+        
+    
+    def step(workspace, model):
+        # Do stuff
+        return 0
