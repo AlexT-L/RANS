e9e9224e93656fd8b593addac5e907210b168bdd
andybroth <ar0535@princeton.edu>
2021-12-06 19:37:04 -0500

    Basic Outline written

---
+++ bin/Workspace.py
@@ -0,0 +1,12 @@
+
+
+class Workspace:
+    
+    # Constructor
+    def __init__(self, grid):
+        self.grid = grid
+        self.fields = []
+        
+    def add_field(self, new_field):
+        # Adds a new field to fields? 
+        return 0
