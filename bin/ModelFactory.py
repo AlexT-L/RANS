90ac6f414e7c22b2a3eea0af78d98bacc0f14e33
andybroth <ar0535@princeton.edu>
2021-12-09 14:25:22 -0500

    Changed model names

--- bin/ModelFactory.py
+++ bin/ModelFactory.py
@@ -1,11 +1,11 @@
-import RANSModel
+import NavierStokes
 
 class ModelFactory:
     
     def __init__(self, modelName, grid, flo_params, coarse):
         self.model = None
-        if modelName == 'RANS':
-            self.model = RANSModel(grid, flo_params, coarse)
+        if modelName == 'Navier':
+            self.model = NavierStokes(grid, flo_params, coarse)
         
         # Raise error if model not selected
     
--- bin/RANSModel.py
+++
@@ -1,6 +0,0 @@
-import Model
-
-class RANSModel(Model):
-    
-    def __init__(self, grid, flo_params, coarse):
-        super().__init__(input)
