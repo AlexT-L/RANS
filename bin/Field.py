d713cd36d4a1641682254d4db5cc878fbc5b16e9
nconlin <nconlin@princeton.edu>
2021-12-09 16:05:43 -0500

    set default ordering of Fields to be fortran compatible

--- bin/Field.py
+++ bin/Field.py
@@ -5,7 +5,7 @@ class Field:
     def __init__(self, grid_size, stateDim=1):
         grid_size.append(stateDim)
         self.dims = grid_size
-        self.vals = np.ones(grid_size)
+        self.vals = np.ones(grid_size, order = 'F') # set fortran ordering for f2py
 
     # Allow fields to be indexed like numpy arrays
     def __getitem__(self,indx): 
