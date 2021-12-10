59832925ed9a51f92b63092fac5b3da2cc38bc30
andybroth <ar0535@princeton.edu>
2021-12-10 14:10:35 -0500

    Making changes to use for inators

--- bin/Field.py
+++ bin/Field.py
@@ -3,7 +3,8 @@ import numpy as np
 class Field:
 
     def __init__(self, grid_size, stateDim=1):
-        grid_size.append(stateDim)
+        if stateDim != 1:
+            grid_size.append(stateDim)
         self.dims = grid_size
         self.vals = np.ones(grid_size, order = 'F') # set fortran ordering for f2py
 
--- bin/Workspace.py
+++ bin/Workspace.py
@@ -7,49 +7,38 @@ class Workspace:
     
     # Constructor
     def __init__(self, grid, isFinest=True):
-        self.grd = grid
+        self.grid = grid
 
         # initialize fields array with Grid fields
-        self.flds = { 'Grid':{} }
-        init_vals = np.zeros(self.grd.dims)
-        for i in range(0,len(self.mdl.reqFields)): # loop over required fields
-            self.flds.Grid[self.mdl.reqFields[i]] = Field(init_vals)
+        self.flds = { 'Grid': {} }
+        dim_vals = np.zeros(self.grid.dims)
+        for i in range(len(self.mdl.reqFields)): # loop over required fields
+            self.flds['Grid'][self.mdl.reqFields[i]] = Field(dim_vals)
 
         self.isFinest = bool(isFinest)
-
-    # return grid object
-    def grid(self):
-        return self.grd
 
     # add field method
     def add_field(self, new_field, fieldName, className='Grid'):
 
         # check if we already have it
-        if fieldName in self.flds[className]:
-            print("Field already exists")
-            return 1
-            
+        if fieldName in list(self.flds[className].keys()):
+            raise ValueError('Field already exists: '+fieldName)
         else: 
             # Add new field to workspace 
             classWorkspace = self.flds[className]
             classWorkspace[fieldName] = new_field
-        
-        return 0
     
     # get field method
     def get_field(self, fieldName, className='Grid'):
 
         # check that field exists
-        if not self.exists(fieldName, className):
-            print("Field doesn't exist")
-            return -1
- 
+        if fieldName not in list(self.flds[className].keys()):
+            raise ValueError('Field does not exist: ' + fieldName) 
         else: 
             # Return field
             classWorkspace = self.flds[className]
             field = classWorkspace[fieldName]
-        
-        return 0
+        return field
 
     def exists(self, fieldName, className='Grid'):
 
