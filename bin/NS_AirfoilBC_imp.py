9f3e34fc3517c5be8bbb37da5265aa6622ca926a
AlexT-L <awt2@princeton.edu>
2021-12-11 20:20:57 -0500

    Update NS_AirfoilBC_imp.py

--- bin/NS_AirfoilBC_imp.py
+++ bin/NS_AirfoilBC_imp.py
@@ -2,56 +2,66 @@ import bcfar_fort, bcwall_fort, halo_fort
 
 class NS_AirfoilBC_imp():
 
-    def bc_far(self, this, workspace, state, p):
+    def bc_far(self, model, workspace, state):
+        # define helper function for getting fields
+        def get(varName):
+            return workspace.get_field(varName, model.className)
+
         # get geometry dictionary
-        geom = workspace.get_geom()
+        geom = workspace.get_geometry()
         
         # dims
-        il = 0
-        jl = 0
-        ie = 0
-        je = 0
-        itl = 0
-        itu = 0
+        [ib, jb] = state.size()
+        il = ib-2
+        jl = jb-2
+        ie = ib-1
+        je = jb-1
+        itl = geom.itl
+        itu = geom.itu
         
         # flo_var
-        w = 0
-        rlv = 0
-        rev = 0
+        w = state.fortran()
+        rlv = get("rlv").fortran()
+        rev = get("rev").fortran()
+        p = get("p").fortran()
         
         # mesh_var
-        x = 0
-        xc = 0
+        coords = workspace.get_field("x")
+        x = coords.fortran()
+        coords = workspace.get_field("xc")
+        xc = coords.fortran()
         
         # out_var
         cp = 0
         cf = 0
         
         # flo_param
-        gamma = 0
-        rm = 0
-        rho0 = 0
-        p0 = 0
-        h0 = 0
-        c0 = 0
-        u0 = 0
-        v0 = 0
-        ca = 0
-        sa = 0
-        re = 0
+        gamma = model.gamma
+        rm = model.rm
+        rho0 = model.rho0
+        p0 = model.p0
+        h0 = model.h0
+        c0 = model.c0
+        u0 = model.u0
+        v0 = model.v0
+        ca = model.ca
+        sa = model.sa
+        re = model.re
         prn = 0
         prt = 0
-        scal = 0
-        chord = 0
-        xm = 0
-        ym = 0
-        kvis = 0
+        scal = geom.scal
+        chord = geom.chord
+        xm = geom.xm
+        ym = geom.ym
+        kvis = model.kvis
         
         # solv_param
         bc = 0
         
         # mg_param
-        mode = 0        
+        mode = 1
+        if workspace.is_finest():
+            mode = 0
         
         bcfar_fort.bcfar(il, jl, ie, je, itl, itu,
                          w, p, rlv, rev,
@@ -62,30 +72,36 @@ class NS_AirfoilBC_imp():
                          mode)
 
 
-    def bc_wall(self, this, workspace, state, pressure, eddyViscosity):
+    def bc_wall(self, model, workspace, state):
+        # define helper function for getting fields
+        def get(varName):
+            return workspace.get_field(varName, model.className)
+
         # get geometry dictionary
-        geom = workspace.get_geom()
+        geom = workspace.get_geometry()
         
         # dims
-        ny = 0
-        il = 0
-        ie = 0
-        ib = 0
-        itl = 0
-        itu = 0
+        [ib, jb] = state.size()
+        nx = ib-3
+        ny = jb-3
+        il = ib-2
+        ie = ib-1
+        itl = geom.itl
+        itu = geom.itu
         
         # flo_var
-        w = state.vals
-        p = pressure.vals
-        rev = eddyViscosity.vals
+        w = state.fortran()
+        p = get("p").fortran()
+        rev = get("rev").fortran()
         
         # mesh_var
-        x = 0
+        coords = workspace.get_field("x")
+        x = coords.fortran()
         
         # flo_param
-        rm = 0
-        sa = 0
-        kvis = 0
+        rm = model.mach
+        sa = model.sa
+        kvis = model.kvis
         
         # solv_param
         isym = 0
@@ -97,34 +113,38 @@ class NS_AirfoilBC_imp():
                            isym)
 
 
-    def halo(self, this, workspace, state, pressure, volume):
+    def halo(self, model, workspace, state):
+        # define helper function for getting fields
+        def get(varName):
+            return workspace.get_field(varName, model.className)
+
         # get geometry dictionary
-        geom = workspace.get_geom()
+        geom = workspace.get_geometry()
         
         # dims
-        il = 0
-        jl = 0
-        ie = 0
-        je = 0
-        ib = 0
-        jb = 0
-        itl = 0
-        itu = 0
+        [ib, jb] = state.size()
+        il = ib-2
+        jl = jb-2
+        ie = ib-1
+        je = jb-1
+        itl = geom.itl
+        itu = geom.itu
         
         # flo_var
-        w = state.vals
-        p = pressure.vals
+        w = state.fortran()
+        p = get("p").fortran()
 
         # mesh_var
-        x = 0
-        vol = volume.vals
+        coords = workspace.get_field("x")
+        x = coords.fortran()
+        vol = get("vol").fortran()
         
         halo_fort.halo(il, jl, ie, je, ib, jb, itl, itu,
              w, p,
              x, vol)
 
     
-    def transfer_down(self, this, workspace1, workspace2, rev1, rlv1, rev2, rlv2):
+    def transfer_down(self, model, workspace1, workspace2, rev1, rlv1, rev2, rlv2):
         # get geometry dictionary
-        geom1 = workspace1.get_geom()
-        geom2 = workspace2.get_geom()
+        geom1 = workspace1.get_geometry()
+        geom2 = workspace2.get_geometry()
