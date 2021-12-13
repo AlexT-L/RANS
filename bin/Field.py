cc3e2d73af5263c15a75dad1c3f7ad20596b7463
AlexT-L <awt2@princeton.edu>
2021-12-12 22:20:19 -0500

    wrote Field math methods

--- bin/Field.py
+++ bin/Field.py
@@ -34,23 +34,79 @@ class Field:
 
     # store the sum of var1 and var2 in self
     def store_sum(self, var1, var2):
-        pass
+        [nx, ny] = self.size()
+
+        for i in range(nx):
+            for j in range(ny):
+                if isinstance(var1, Field):
+                    if isinstance(var2, Field):
+                        self[i,j] = var1[i,j] + var2[i,j]
+                    else:
+                        self[i,j] = var1[i,j] + var2
+                else:
+                    if isinstance(var2, Field):
+                        self[i,j] = var1 + var2[i,j]
+                    else:
+                        self[i,j] = var1 + var2
 
     # store the difference (var1 - var2) in self
     def store_difference(self, var1, var2):
-        pass
+        [nx, ny] = self.size()
+
+        for i in range(nx):
+            for j in range(ny):
+                if isinstance(var1, Field):
+                    if isinstance(var2, Field):
+                        self[i,j] = var1[i,j] - var2[i,j]
+                    else:
+                        self[i,j] = var1[i,j] - var2
+                else:
+                    if isinstance(var2, Field):
+                        self[i,j] = var1 - var2[i,j]
+                    else:
+                        self[i,j] = var1 - var2
 
     # store the elementwise product of var1 and var2 in self
     def store_product(self, var1, var2):
-        pass
+        [nx, ny] = self.size()
+
+        for i in range(nx):
+            for j in range(ny):
+                if isinstance(var1, Field):
+                    if isinstance(var2, Field):
+                        self[i,j] = var1[i,j] * var2[i,j]
+                    else:
+                        self[i,j] = var1[i,j] * var2
+                else:
+                    if isinstance(var2, Field):
+                        self[i,j] = var1 * var2[i,j]
+                    else:
+                        self[i,j] = var1 * var2
 
     # store the elementwise quotient (var1/var2) in self
     def store_quotient(self, var1, var2):
-        pass
+        [nx, ny] = self.size()
+
+        for i in range(nx):
+            for j in range(ny):
+                if isinstance(var1, Field):
+                    if isinstance(var2, Field):
+                        self[i,j] = var1[i,j] / var2[i,j]
+                    else:
+                        self[i,j] = var1[i,j] / var2
+                else:
+                    if isinstance(var2, Field):
+                        self[i,j] = var1 / var2[i,j]
+                    else:
+                        self[i,j] = var1 / var2
 
     # elementwise copy self into copy
     def copy_to(self, copy):
-        pass
+        [nx, ny] = self.size()
+
+        for i in range(nx):
+            for j in range(ny):
+                copy[i,j] = self[i,j]
 
     # elementwise multiply self by k (could be field or scalar)
     def scale(self, k):
