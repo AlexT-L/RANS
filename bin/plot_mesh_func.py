ba0a683daae51bdca65b8431c2c49a55266df956
Vedin Dewan <vedindewan@dynamic-oit-vapornet100-10-9-112-240.princeton.edu>
2021-12-13 11:59:31 -0500

    added function to plot mesh

---
+++ bin/plot_mesh_func.py
@@ -0,0 +1,17 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+def plot_mesh(self,ver):
+    #x-y vertices
+    x=ver[:,:,0]
+    y=ver[:,:,1]
+    #transpose of x-y vertices
+    x_t=x.T 
+    y_t=y.T 
+    #plot c-mesh
+    plt.plot(x,y)
+    plt.plot(x_t,y_t,linewidth="0.5")
+    plt.axis([-0.75,1.50,-0.8,0.8])
+    plt.show()
+
+    return
