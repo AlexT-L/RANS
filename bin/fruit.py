458680d87ddadaa7aa301be65c406a3d0c725f82
Satya Butler <sb3137@princeton.edu>
2021-12-12 20:52:09 -0500

    fruity templates

---
+++ bin/fruit.py
@@ -0,0 +1,37 @@
+"""
+Example program.
+
+Description
+-----------
+Example program with proper comment styles.
+
+Libraries/Modules
+-----------------
+None.
+
+Notes
+-----
+Also none.
+
+Author(s)
+---------
+Satya, Joey Salads, V, Alex, My sexuality is rich people/dank meme Andy, and Brian. \n
+Created on 12/11/2021. \n
+Last modified on 12/12/2021.
+
+"""
+
+def bananas(fruit):
+    """Description of 'bananas' method goes here.
+    
+    Parameters
+    ----------
+    fruit:
+        Description of 'fruit' parameter goes here.
+
+    Returns
+    -------
+    :
+        Description of return value (here: the string 'bananas').
+    """
+    return "bananas"
---
+++ bin/tests/test_fruit.py
@@ -0,0 +1,41 @@
+"""
+Example test program.
+
+Description
+-----------
+Example pytest program with proper comment styles.
+
+Libraries/Modules
+-----------------
+-pytest \n
+-fruit (local)
+
+Notes
+-----
+Here's a note.
+
+Author(s)
+---------
+Satya, Joey Salads, V, Alex, My sexuality is rich people/dank meme Andy, and Brian. \n
+Created on 12/11/2021. \n
+Last modified on 12/12/2021.
+
+"""
+import pytest
+from .. import fruit
+
+def test_bananas():
+    """
+    Description of 'test_bananas' method goes here.
+
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    :
+        Nothing, but it asserts if 'this_is_bananas' is in fact the string 'bananas'.
+    
+    """
+    this_is_bananas = fruit.bananas()
+    assert this_is_bananas == "bananas"
