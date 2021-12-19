261bed128eaf91afe5e8094658e3b453fbd7a474
Satya Butler <sb3137@princeton.edu>
2021-12-19 14:18:43 -0500

    much

--- bin/Contractinator.py
+++ bin/Contractinator.py
@@ -14,7 +14,7 @@ def simple(fine, coarse):
     if type(fine) is not Field or type(coarse) is not Field:
         raise TypeError('Fine or coarse field is not a field')
     
-    #[x_fine, y_fine, dim] = fine.shape()
+    #[x_fine, y_fine, dim] = fine.shape() # Don't need 'dim'. In fact, it causes errors.
     [x_fine, y_fine] = fine.shape()
 
     # get slice indices
