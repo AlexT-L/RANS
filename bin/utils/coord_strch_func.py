d456f99caf3d637cb7af2eea18b2f56d3be41aa9
Vedin Dewan <vedindewan@dynamic-oit-vapornet100-10-9-112-240.princeton.edu>
2021-12-19 14:49:35 -0500

    removedplot lines

--- bin/utils/coord_strch_func.py
+++ bin/utils/coord_strch_func.py
@@ -71,6 +71,7 @@ def coord_stretch(self):
         g         = 1.  -d*d
         c         = g**ay
         b0[i]     = sy*d/c
+    
     #print("STRETCH")
     #print("A0=",a[0])
     #print("B0=",b0)
