700ee75907c0841f2185282944cbed49599eff9b
BWynnePU <90227306+BWynnePU@users.noreply.github.com>
2021-12-19 14:44:37 -0500

    Visc: Finished profiling updating, switching loops to numpy vector for most cases, now all visc files run faster. Added doc strings. Updated gitignore to stop profiling output.

--- bin/model_funcs/Viscosity.py
+++ bin/model_funcs/Viscosity.py
@@ -9,9 +9,13 @@ from BoundaryThickness import boundary_thickness
 
 # @profile
 def compute_viscosity(params, dims):
-    #  from subroutine viscf.f
-    print('start of compute_viscosity')
-    # computes viscosity coefficients               
+    z=0
+    '''
+     from subroutine viscf.f
+    '''
+    '''
+    computes viscosity coefficients  
+    '''             
     ie = params['ie'] # Mesh dimension
     je = params['je'] # Mesh dimension
     kvis = params['kvis']
@@ -46,8 +50,8 @@ def compute_viscosity(params, dims):
     # initializing, defined later
     rev0 = np.ones((dim_var,dim_var))
     astr = np.ones((dim_var,dim_var))
-    u = []
-    v = []
+    u = np.ones((dim_var,dim_var))
+    v = np.ones((dim_var,dim_var))
 
     # useful constants
     # ckr       = (.062/(2.*pi)**4)
@@ -61,11 +65,11 @@ def compute_viscosity(params, dims):
     visc_const_C2 = 110.3
 
     # compute the molecular viscosity
+    # i>1:il, i+1>2:il+1, i-1>0:il-1
+    # j>1:jlm, j+1>2:jlm+1, j-1>0:jlm-1
 
-    for j in range(0,je):
-        for i in range(0,ie):
-            tt       = p[i,j]/w[i,j,0]*t0
-            rlv[i,j] = visc_const_C1*tt*np.sqrt(tt)/((tt+visc_const_C2)*rmu0)
+    tt       = p[0:ie,0:je]/w[0:ie,0:je,0]*t0
+    rlv[0:ie,0:je] = visc_const_C1*tt*np.sqrt(tt)/((tt+visc_const_C2)*rmu0)
 
 
     # for laminar flows we are done.
@@ -79,52 +83,53 @@ def compute_viscosity(params, dims):
     if (ncyc > 25):
         aturb = .5
     if (kturb == 1): # if kturb is one, else  
-        for j in range(0,je):
-            for i in range(0,ie):
-                rev0[i,j] = rev[i,j]
+        rev0[0:ie,0:je] = rev[0:ie,0:je]
         # call turbbl
         # call turb2
+        '''
+        If running laminar flows, calculation is more simple.
+        '''
         # runs the tubr visc calculations
+        '''
+        Call either the Baldwin Lomax Model or run the RNG algebraic model.
+        '''
         turbulent_viscosity(params, dims)
 
-        for j in range(0,je):
-            for i in range(0,ie):
-                    rev[i,j] = aturb*rev[i,j]  +(1.  -aturb)*rev0[i,j]
+
+        rev[0:ie,0:je] = aturb*rev[0:ie,0:je]  +(1.  -aturb)*rev0[0:ie,0:je]
     #  else start the rng algebraic model
     else:
-        for j in range(0,je):
-            for i in range(0,ie):
-                u[i,j]   = w[i,j,1]/w[i,j,0]
-                v[i,j]   = w[i,j,2]/w[i,j,0]
+    # i>1:il, i+1>2:il+1, i-1>0:il-1
+    # j>1:jlm, j+1>2:jlm+1, j-1>0:jlm-1
 
+        u[0:ie,0:je]   = w[0:ie,0:je,1]/w[0:ie,0:je,0]
+        v[0:ie,0:je]   = w[0:ie,0:je,2]/w[0:ie,0:je,0]
 
-            for i in range(itl,itu):
-                u[i,0]   = -u[i,1]
-                v[i,0]   = -v[i,1]
-        for j in range(0,jl):
-            for i in range(0,il):
+        u[itl:itu,0]   = -u[itl:itu,1]
+        v[itl:itu,0]   = -v[itl:itu,1]
 
-                dx13      = xc[i,j,0]   - xc[i+1,j+1,0]
-                dy13      = xc[i,j,1]   - xc[i+1,j+1,1]
-                dx24      = xc[i+1,j,0] - xc[i,j+1,0]
-                dy24      = xc[i+1,j,1] - xc[i,j+1,1]
-                du13      = u[i,j] - u[i+1,j+1]
-                dv13      = v[i,j] - v[i+1,j+1]
-                du24      = u[i+1,j] - u[i,j+1]
-                dv24      = v[i+1,j] - v[i,j+1]
-                # ua        = .25*(u[i,j] + u[i+1,j+1] + u[i+1,j] + u[i,j+1])
-                # va        = .25*(v[i,j] + v[i+1,j+1] + v[i+1,j] + v[i,j+1])
-                dsij      = 1./(dx13*dy24 - dx24*dy13)
-                dvdx      =  dsij * (dv13*dy24 - dv24*dy13)
-                dudy      = -dsij * (du13*dx24 - du24*dx13)
-                dudx      =  dsij * (du13*dy24 - du24*dy13)
-                dvdy      = -dsij * (dv13*dx24 - dv24*dx13)
-                astr[i,j] = (dudy+dvdx)**2. +2.*(dudx**2  +dvdy**2  -((dudx+dvdy)**2)/3.)
-
+        dx13      = xc[0:il,0:jl,0]   - xc[1:il+1,1:jl+1,0]
+        dy13      = xc[0:il,0:jl,1]   - xc[1:il+1,1:jl+1,1]
+        dx24      = xc[1:il+1,0:jl,0] - xc[0:il,1:jl+1,0]
+        dy24      = xc[1:il+1,0:jl,1] - xc[0:il,1:jl+1,1]
+        du13      = u[0:il,0:jl] - u[1:il+1,1:jl+1]
+        dv13      = v[0:il,0:jl] - v[1:il+1,1:jl+1]
+        du24      = u[1:il+1,0:jl] - u[0:il,1:jl+1]
+        dv24      = v[1:il+1,0:jl] - v[0:il,1:jl+1]
+        # ua        = .25*(u[i,j] + u[i+1,j+1] + u[i+1,j] + u[i,j+1])
+        # va        = .25*(v[i,j] + v[i+1,j+1] + v[i+1,j] + v[i,j+1])
+        dsij      = 1./(dx13*dy24 - dx24*dy13)
+        dvdx      =  dsij * (dv13*dy24 - dv24*dy13)
+        dudy      = -dsij * (du13*dx24 - du24*dx13)
+        dudx      =  dsij * (du13*dy24 - du24*dy13)
+        dvdy      = -dsij * (dv13*dx24 - dv24*dx13)
+        astr[0:il,0:jl] = (dudy+dvdx)**2. +2.*(dudx**2  +dvdy**2  -((dudx+dvdy)**2)/3.)
 
         #   to add: 
         # call delt
-    # calculates the boundary layer thickness
+    '''
+    Calculates the boundary layer thickness.
+    '''
     boundary_thickness(params, dims) 
 
 
@@ -153,7 +158,9 @@ def compute_viscosity(params, dims):
             a1        = a11*(astra)
             rnut0     = rnul+rnut0
 
-        #     solve for the eddy viscosity
+            '''
+            Solves for the eddy viscosity
+            '''
             # DIM(X,Y) function in fortran: returns the difference X-Y if the result is positive; otherwise returns zero.
             # Replacing with: max(X-Y, 0) for python
             
@@ -167,6 +174,7 @@ def compute_viscosity(params, dims):
             fac    = a2 - 1.
 
             while k<201: 
+                z=z+1
                 den    = 1./(4.*rnut*rnut*rnut + fac)
                 rnut1  = rnut - (rnut**4+rnut*fac  -rnut0*rnut0*a1)*den
                 
@@ -175,7 +183,7 @@ def compute_viscosity(params, dims):
                     break # exits the while loop, then continues
                 else:
                     k      = k  +1
-                    if (k>200):
+                    if (k>199):
                         print([' iteration not converged ',i,j])
                         print([' rnut = ',rnut,' rnut1 =',rnut1])
 
@@ -194,10 +202,10 @@ def compute_viscosity(params, dims):
             pex       = -(xc[ii,1,0]  -xc[itu,1,0])/(20.*dsti[itu])
             rev[ii,j] = rev[ii,j]  +(rev[itu,j]  -rev[ii,j])*np.exp(pex)
 
-    for i in range(1,il):
-            ii        = ib  -i
-            rev[i,je] = rev[i,jl]
-            rev[i,0]  = rev[ii,1]
+    ii        = ib  -i
+            
+    rev[1:il,je] = rev[1:il,jl]
+    rev[1:il,0]  = rev[ii,1]
 
     for i in range(itl,itu):
             if (xc[i,1,0] <= xtran):
@@ -205,9 +213,9 @@ def compute_viscosity(params, dims):
                     rev[i,j]  = 0
             rev[i,1]  = -rev[i,2]
 
-    for j in range(0,je):
-            rev[0,j]  = rev[1,j]
-            rev[ie,j] = rev[il,j]
+    rev[0,0:je]  = rev[1,0:je]
+    rev[ie,0:je] = rev[il,0:je]
+
     return
 dim_var = 500
 params = {
@@ -234,7 +242,7 @@ params = {
   "p" : np.ones((dim_var,dim_var)),
   "mode": 0,
   "kturb": 1,
-  "xc": np.ones((dim_var,dim_var,3))*2,
+  "xc": np.random.rand(dim_var,dim_var,3),
   "ynot": np.ones(dim_var),
   "rlv": np.ones((dim_var,dim_var)),
   "dsti": np.ones(dim_var),
