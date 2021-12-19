e9e0754ad05e788ee43a2a1bcec96b705d8dedde
Vedin Dewan <vedindewan@dynamic-oit-vapornet100-10-9-112-240.princeton.edu>
2021-12-19 14:48:58 -0500

    removedplot lines

--- bin/utils/plot_mesh_func.py
+++ bin/utils/plot_mesh_func.py
@@ -12,6 +12,8 @@ def plot_mesh(self):
     #plot c-mesh
     plt.plot(x.get_vals(),y.get_vals())
     plt.plot(x_t,y_t,linewidth="0.5")
+    plt.xlabel("x [chord lengths]",fontsize=15)
+    plt.ylabel("y [chord lengths]",fontsize=15)
     plt.axis([-0.75,1.50,-0.8,0.8])
     plt.show()
 
