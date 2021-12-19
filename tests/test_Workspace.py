90d3c2a55695162e1250af76c40b34d421b4f004
BWynnePU <90227306+BWynnePU@users.noreply.github.com>
2021-12-19 16:05:02 -0500

    Updated test for Workspace, added inputs same as flo103

--- tests/test_Workspace.py
+++ tests/test_Workspace.py
@@ -18,9 +18,16 @@ then check that init_vars? exists return True
 check that has_dict? exists and has_dict return false when they should
 check is_finest()
 """
+import sys
+# sys.path.append('../../)
+sys.path.append('../../RANS/bin')
 from numpy.core.numeric import NaN
 import pytest
-from bin.Workspace import Workspace
+from Input import Input
+from Field import Field
+from AirfoilMap import AirfoilMap
+from Workspace import Workspace
+from CellCenterWS import CellCenterWS
 import numpy as np
 
 
@@ -28,7 +35,18 @@ def test_x():
     """
     Asserts that we can retrieve x and that it is non zero
     """
-    x = Workspace.get_field('x')
+    # input = Input('rae9-s1.data') # Will actually take all command line inputs
+    filename = 'rae9-s1.data'
+    # read in input
+    input = Input(filename) # Will actually take all command line inputs
+    # format input
+    input.geo_param["inflation_layer"] = (input.flo_param["kvis"] != 0)
+    gridInput = input.add_dicts(input.geo_param, input.in_var)
+    grid_dim = [input.dims['nx'], input.dims['ny']]
+    # create geometry objects
+    grid = AirfoilMap.from_file(grid_dim, gridInput)
+    workspace = CellCenterWS(grid)
+    x = workspace.get_field('x')
     # Asserts non zero, as np.any is true when any are non-zero
     assert np.any(x)
 
@@ -36,7 +54,18 @@ def test_xc():
     """
     Asserts that we can retrieve xc and that it is non zero
     """
-    xc = Workspace.get_field('xc')
+    # input = Input('rae9-s1.data') # Will actually take all command line inputs
+    filename = 'rae9-s1.data'
+    # read in input
+    input = Input(filename) # Will actually take all command line inputs
+    # format input
+    input.geo_param["inflation_layer"] = (input.flo_param["kvis"] != 0)
+    gridInput = input.add_dicts(input.geo_param, input.in_var)
+    grid_dim = [input.dims['nx'], input.dims['ny']]
+    # create geometry objects
+    grid = AirfoilMap.from_file(grid_dim, gridInput)
+    workspace = CellCenterWS(grid)
+    xc = workspace.get_field('xc')
     # Asserts non zero, as np.any is true when any are non-zero
     assert np.any(xc)
 
@@ -44,23 +73,37 @@ def test_vol():
     """
     Asserts that we can retrieve vol and that it is non zero
     """
-    vol = Workspace.get_field('vol')
+    # input = Input('rae9-s1.data') # Will actually take all command line inputs
+    filename = 'rae9-s1.data'
+    # read in input
+    input = Input(filename) # Will actually take all command line inputs
+    # format input
+    input.geo_param["inflation_layer"] = (input.flo_param["kvis"] != 0)
+    gridInput = input.add_dicts(input.geo_param, input.in_var)
+    grid_dim = [input.dims['nx'], input.dims['ny']]
+    # create geometry objects
+    grid = AirfoilMap.from_file(grid_dim, gridInput)
+    workspace = CellCenterWS(grid)
+    vol = workspace.get_field('vol')
     # Asserts non zero, as np.any is true when any are non-zero
     assert np.any(vol)
 
-def test_init_vars():
-    '''
-    Verify that init_vars works as expected
-    then check that init_vars? exists return True
-    '''
-    # not sure about this yet
-    a=True
-    assert a
+# def test_init_vars():
+#     '''
+#     Verify that init_vars works as expected
+#     then check that init_vars? exists return True
+#     '''
+#     # not sure about this yet
+#     a=True
+#     assert a
 
-def test_finest():
-    finest_check= Workspace.isFinest
-    '''
-    Asserts that isFinest 
-    '''
-    # but not always? cross check with mode?
-    assert finest_check
+# def test_finest():
+#     finest_check= Workspace.isFinest
+#     '''
+#     Asserts that isFinest 
+#     '''
+#     # but not always? cross check with mode?
+#     assert finest_check
+
+
+test_x()
