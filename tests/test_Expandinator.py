43e1b7d169d7b723dc60759c14b61ce8ec8a7c97
Satya Butler <sb3137@princeton.edu>
2021-12-20 00:19:41 -0500

    comments, testing

--- tests/test_Expandinator.py
+++ tests/test_Expandinator.py
@@ -26,7 +26,8 @@ import bin.Expandinator as Exp
 
 def test_bilinear4way():
     """
-    Tests the Expandinator.py 'bilinear4way' method. Does not currently assert (test) anything.
+    Tests the Expandinator.py 'bilinear4way' method. Does not currently assert (test) anything
+    as Expandinator.py does not currently work.
 
     Parameters
     ----------
@@ -37,33 +38,43 @@ def test_bilinear4way():
         Nothing, but asserts if 'bilinear4way' expands a Field properly.
     
     """
-    output_dims = (4,4)
-    output_values = np.array([[1,3,1,3],[1,3,1,3],[1,3,1,3],[1,3,1,3]])
-    output_field = Field(output_dims, output_values)
-    #coarse_dims = (2,2)
-    #coarse_field = Field(coarse_dims)
-    input_dims = (2,2)
-    input_values = np.array([[2,2],[2,2]])
-    input_field = Field(input_dims, input_values)
+    #output_dims = (4,4)
+    #output_values = np.array([[1,3,1,3],[1,3,1,3],[1,3,1,3],[1,3,1,3]])
+    #output_field = Field(output_dims, output_values)
+    ##coarse_dims = (2,2)
+    ##coarse_field = Field(coarse_dims)
+    #input_dims = (2,2)
+    #input_values = np.array([[2,2],[2,2]])
+    #input_field = Field(input_dims, input_values)
+
 
     test_dims = (2,2,4)
-    test_values = np.zeros(test_dims)
+    #test_dims = (2,2)
+    test_values = np.zeros(test_dims) + 1
     test_field = Field(test_dims, test_values)
     print(test_field)
-    Exp.bilinear4way(test_field, test_field) # As of writing, first test_field doesn't get used 
-                                             # (is redundant, but needed for function call)
-    print(test_field)
+
+    testfine_dims = (4,4,4)
+    #testfine_dims = (4,4)
+    testfine_values = np.zeros(testfine_dims)
+    testfine_field = Field(testfine_dims, testfine_values)
+    print(testfine_field)
 
     testoutput_dims = (4,4,4)
-    testoutput_values = np.zeros(testoutput_dims)
+    #testoutput_dims = (4,4)
+    testoutput_values = np.zeros(testoutput_dims) + 1
     testoutput_field = Field(testoutput_dims, testoutput_values)
     print(testoutput_field)
+#    Exp.bilinear4way(test_field, testfine_field)
+    print("bananas")
+    print(testfine_field)
+    print(testoutput_field)
 
-#    assert array_equal(test_field, testoutput_field)
+#    assert array_equal(testfine_field, testoutput_field)
 
-    dims = (2,2,2)
-    values = np.array([[[2,2],[2,2]],[[2,2],[2,2]]])
-    field = Field(dims, values)
+    #dims = (2,2,2)
+    #values = np.array([[[2,2],[2,2]],[[2,2],[2,2]]])
+    #ield = Field(dims, values)
     #print(field)
 
     #Exp.bilinear4way(input_field, field)
