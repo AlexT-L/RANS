80d8301b21e3349488bf9d2e27acb6df00748352
nconlin <nconlin@princeton.edu>
2021-12-19 21:50:22 -0600

    added doc strings

--- bin/Cycle.py
+++ bin/Cycle.py
@@ -1,33 +1,38 @@
-"""
-Cycle.py 
-
-Description
------------
-Class describing multigrid cycle. \n
-Contains information on cycle shape and depth.
-
-Libraries/Modules
------------------
-None.
-
-Notes
------
-Could be expanded to include standard options like "V" and "W" cycle
-
-Author(s)
----------
-Satya Butler, Nick Conlin, Vedin Dewan, Andy Rothstein, Alex Taylor-Lash, and Brian Wynne. \n
-
-"""
-
 from abc import ABC
 import numpy as np
 
 class Cycle(ABC):
+    """
+    Description
+    -----------
+    Class describing multigrid cycle.
+    Contains information on cycle shape and depth.
+
+    Attributes
+    ----------------
+    pattern:
+        sequence of directions to perform a multigrid cycle
+
+    levels:
+        depth of multigrid cycle
+
+    Libraries/Modules
+    -----------------
+    numpy
+    abc
+
+    Notes
+    -----
+    Could be expanded to include standard options like "V" and "W" cycle
+
+    Author(s)
+    ---------
+    Satya Butler, Nick Conlin, Vedin Dewan, Andy Rothstein, Alex Taylor-Lash, and Brian Wynne. \n
+
+    """
     
     # Constructor
     def __init__(self, input):
-        self.grids = []
         self.pattern = [-1,-1,1,-1,1,1]
         self.levels = 3
 
