2206df2c65e231c103b1bc8e0a0a8432ac652b4c
BWynnePU <90227306+BWynnePU@users.noreply.github.com>
2021-12-19 23:59:50 -0500

    Updating Workspace/floConvg and flowPost to new documentation format

--- bin/flo103_PostProcessor.py
+++ bin/flo103_PostProcessor.py
@@ -1,30 +1,25 @@
+"""This would provide postprocessing of results. 
 
+    Libraries/Modules:
+        Would use: numpy\n
+        Would use: pandas\n
+        """
 
 class flo103_PostProcessor:
-    '''
-    Description
-    -----------
-    Not implemented as this time. 
-    What it would do: 
-    Plot pressure profile and entropy potential lines.
-    Plot outputs of wall stress and aerodynamics of airfoil.
+    """Not implemented as this time. 
+        What it would do: 
+        Plot pressure profile and entropy potential lines.
+        Plot outputs of wall stress and aerodynamics of airfoil.
 
-    Attributes
-    -----------------
-    None currently used.
+    Attributes:
+        None currently used.
 
-    Libraries/Modules
-    -----------------
-    Would use: matplotlib
-    Would use: pandas
-
-    Notes
-    -----
+    Notes: 
     Currently just passed when called. 
-    '''
+    """
 
     def __init__(self, input):
-        '''
+        """
         Is not used in current implementation. 
-        '''
+        """
         pass
