Debugging code (now runs, but getting wrong values)

bin/model_funcs/Viscosity.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append('../../../RANS/bin')
 
 import numpy as np
-from bin.Field import pos_diff
+from bin.Field import pos_diff, max, min
 from bin.model_funcs.BaldwinLomax import turbulent_viscosity
 from bin.model_funcs.BoundaryThickness import boundary_thickness
 
@@ -99,7 +99,6 @@ def compute_viscosity(model, ws, state):
     # compute the molecular viscosity
     tt       = p[1:ie+1,1:je+1]/w[1:ie+1,1:je+1,0]*t0
     rlv[1:ie+1,1:je+1] = visc_const_C1*tt*np.sqrt(tt)/((tt+visc_const_C2)*rmu0)
-
 
     # for laminar flows we are done.
     # for turbulent flows we are also done on the coarser grids.

bin/model_funcs/stability_fast.py
@@ -1,5 +1,5 @@
 import numpy as np
-from bin.Field import Field, copy, maximum, minimum, abs, pos_diff, sqrt, square, min
+from bin.Field import Field, copy, isfinite, maximum, minimum, abs, pos_diff, sqrt, square, min
 
 def stability(self, model, workspace, state):
     """Calculates timestep limits to maintain stability
@@ -70,6 +70,10 @@ def stability(self, model, workspace, state):
     w = state
     p = get("p")
     vol = get("vol")
+    
+    lv = 1e5*np.ones(lv.shape)
+    
+    assert isfinite(radI)
 
     # local working arrays
     def get_local(varName):
@@ -155,6 +159,8 @@ def stability(self, model, workspace, state):
         rfli[ip:ie, jp:je] = radI[ip:ie, jp:je]/dtlc[ip:ie, jp:je]
         rflj[ip:ie, jp:je] = radJ[ip:ie, jp:je]/dtlc[ip:ie, jp:je]
     
+    assert isfinite(radI)
+
     # c
     # c
     # c     reduce the artificial dissipation for viscous flows
@@ -179,6 +185,9 @@ def stability(self, model, workspace, state):
         dtl[ip:ie, jp:je] = 1/dtv
         radI[ip:ie, jp:je] = pos_diff(radI[ip:ie, jp:je], vsi)
         radJ[ip:ie, jp:je] = pos_diff(radJ[ip:ie, jp:je], vsj)
+
+    assert isfinite(lv)
+    assert isfinite(radI)
 
     # c
     # c     set boundary values at i=1 and i=ie