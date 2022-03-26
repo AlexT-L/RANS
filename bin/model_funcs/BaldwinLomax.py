fixed indexing and allocation in viscosity calculation

bin/model_funcs/BaldwinLomax.py
@@ -3,7 +3,11 @@
     Libraries/Modules:
         numpy\n
         """
+import sys
+sys.path.append('../RANS/bin')
+
 import numpy as np
+from bin.Field import maximum, minimum, create
 # from Grid import Grid
 
 # class BaldwinLomax():
@@ -27,6 +31,7 @@ def turbulent_viscosity(model, ws, state):
         Adapted from subroutine turb2.f
     """
     [nx, ny] = ws.field_size()
+    PAD = model.padding
     # necessary fields
     def wsget(varName):
         return ws.get_field(varName, model.className)
@@ -52,37 +57,40 @@ def turbulent_viscosity(model, ws, state):
     xtran = mget('xtran') 
     vol = wsget('vol') 
 
-    # tauw = np.ones(nx)
-    # yscal = np.ones(nx)
-    # vor = np.ones((nx,ny))
-    # avorm = np.ones(nx)
-    # ravg = np.ones(nx)
+    tauw = np.zeros(nxp)
+    yscal = 1000000*np.ones(nxp)
+    vor = np.zeros((nxp,nyp))
+    avor = np.zeros(nyp)
+    avorm = np.zeros(nxp)
+    ravg = np.ones(nyp)
     amu = np.ones((nxp,nyp))
     amut = np.ones((nxp,nyp))
-    # amuto = np.ones(nx)
-    # amuti = np.ones(nx)
-    # yvor = np.ones(nx)
-    # yvorm = np.ones(nx)
-    # utot = np.ones((nx,ny))
-    # utotm = np.ones(nx)
-    # utmin = np.ones(nx)
-    # fkleb = np.ones(nx)
-    # jedge = np.ones(nx)
-    # utmax = np.ones(nx)
+    amuto = np.ones(nyp)
+    amuti = np.ones(nyp)
+    yvor = np.ones(nyp)
+    yvorm = np.ones(nxp)
+    utot = np.zeros((nxp,nyp))
+    utot1 = np.zeros(nyp)
+    utotm = np.zeros(nxp)
+    utmin = np.zeros(nxp)
+    utmax = np.zeros(nxp)
+    fkleb = np.ones(nyp)
+    jedge = np.ones(nxp)
     u = np.ones((nxp,nyp))
     v = np.ones((nxp,nyp))
     t = np.ones((nxp,nyp))
-    # fcros = np.ones(nx)
+    fcros = np.ones(nyp)
     rinv = np.ones((nxp,nyp))
-    # vola = np.ones((nx,ny))
-    # ylen = np.ones((nx,ny))
-    # ylenm = np.ones(nx)
+    vola = np.ones((nxp,nyp))
+    ylen = np.ones((nxp,nyp))
+    ylenm = np.ones(nxp)
 
+    i2        = ie
     j2        = je
     jlm       = jl- 1
 
-    jstop     = 3* (j2- 2)/5
-    itlp      = itl+ 1
+    jstop     = (int)(3* (j2- 2)/5)
+    itlp      = itl+ 1    
 
     cwk1      = 1.0
     ckleb     = 0.3
@@ -95,12 +103,12 @@ def turbulent_viscosity(model, ws, state):
     sgrm      = sgam*rm
     sgrmi     = 1.0/sgrm
 
-    rinv[:,:] = 1.0/w[:,:,0]
-    t[:,:]    = p[:,:]* rinv[:,:]
-    u[:,:]    = w[:,:,1]* rinv[:,:]
-    v[:,:]    = w[:,:,2]* rinv[:,:]
-    amu[:,:]  = t[:,:]
-    amut[:,:] = 0.0
+    rinv[1:i2+1,1:j2+1] = 1.0/w[1:i2+1,1:j2+1,0]
+    t[1:i2+1,1:j2+1]    = p[1:i2+1,1:j2+1]* rinv[1:i2+1,1:j2+1]
+    u[1:i2+1,1:j2+1]    = w[1:i2+1,1:j2+1,1]* rinv[1:i2+1,1:j2+1]
+    v[1:i2+1,1:j2+1]    = w[1:i2+1,1:j2+1,2]* rinv[1:i2+1,1:j2+1]
+    amu[1:i2+1,1:j2+1]  = t[1:i2+1,1:j2+1]
+    amut[1:i2+1,1:j2+1] = 0.0
 
     '''
     Determination of eddy viscosity
@@ -110,57 +118,57 @@ def turbulent_viscosity(model, ws, state):
     Calculates vorticity and total velocity
     '''
 
-    vola[:,:] = 0.5* (vol[:,:]+ vol[:,:+1])
+    vola[1:i2+1,1:jl+1] = 0.5* (vol[1:i2+1,1:jl+1]+ vol[1:i2+1,2:jl+2])
 
-    xxa = x[1:,1,1]-x[:il,1,1] 
-    yxa = x[1:,1,2]-x[:il,1,2]
-    uy        = u[1:,2]- u[1:,1]
-    vy        = v[1:,2]- v[1:,1]
-    uavg      = .5* (u[1:,1]+u[1:,2])
-    vavg      = .5* (v[1:,1]+v[1:,2])
+    xxa = x[2:il+1,1,1]-x[1:il,1,1] 
+    yxa = x[2:il+1,1,2]-x[1:il,1,2]
+    uy        = u[2:il+1,2]- u[2:il+1,1]
+    vy        = v[2:il+1,2]- v[2:il+1,1]
+    uavg      = 0.5* (u[2:il+1,1] + u[2:il+1,2])
+    vavg      = 0.5* (v[2:il+1,1] + v[2:il+1,2])
 
 
-    vor1      = (xxa*uy + yxa*vy)/vola[1:,1]
+    vor1      = (xxa*uy + yxa*vy)/vola[2:il+1,1]
     vor2      = 0.0
     vor3      = 0.0
 
     vort      = vor1-vor2-vor3
-    vor[1:,1]  = abs(vort)
+    vor[2:il+1,1]  = abs(vort)
     utotal    = uavg*uavg + vavg*vavg
 
-    utot[1:,1] = np.sqrt(utotal)
+    utot[2:il+1,1] = np.sqrt(utotal)
 
 
-    xxa       = x[1:il,1:jlm,0]-x[0:il-1,1:jlm,0] 
-    yxa       = x[1:il,1:jlm,1]-x[0:il-1,1:jlm,1] 
-    uy        = u[1:il,2:jlm+1]- u[1:il,1:jlm]
-    vy        = v[1:il,2:jlm+1]- v[1:il,1:jlm]
-    uavg      = 0.5* (u[1:il,1:jlm]+ u[1:il,2:jlm+1])
-    vavg      = 0.5* (v[1:il,1:jlm]+ v[1:il,2:jlm+1])
+    xxa       = x[2:il+1,2:jlm+1,0]-x[1:il,2:jlm+1,0] 
+    yxa       = x[2:il+1,2:jlm+1,1]-x[1:il,2:jlm+1,1] 
+    uy        = u[2:il+1,3:jlm+2]- u[2:il+1,2:jlm+1]
+    vy        = v[2:il+1,3:jlm+2]- v[2:il+1,2:jlm+1]
+    uavg      = 0.5* (u[2:il+1,2:jlm+1]+ u[2:il+1,3:jlm+2])
+    vavg      = 0.5* (v[2:il+1,2:jlm+1]+ v[2:il+1,3:jlm+2])
     '''
     thin-layer navier-stokes contribution to vorticity
     '''
-    vor1      = (xxa*uy + yxa*vy)/vola[1:il,1:jlm]
+    vor1      = (xxa*uy + yxa*vy)/vola[2:il+1,2:jlm+1]
     '''
     additional contributions to vorticity
     '''
-    xyw       = 0.5* (x[0:il-1,2:jlm+1,0]- x[0:il-1,0:jlm-1,0])
-    xye       = 0.5* (x[1:il,2:jlm+1,0]- x[1:il,0:jlm-1,0])
-    yyw       = 0.5* (x[0:il-1,2:jlm+1,1]- x[0:il-1,0:jlm-1,1])
-    yye       = 0.5* (x[1:il,2:jlm+1,1]- x[1:il,0:jlm-1,1])
-    volawi    = 2.0/(vola[1:il,1:jlm]+ vola[0:il-1,1:jlm])
-    volaei    = 2.0/(vola[1:il,1:jlm]+ vola[2:il+1,1:jlm])
-    uxe       = 0.5* (u[2:il+1,1:jlm]+u[2:il+1,2:jlm+1]) - uavg
-    vxe       = 0.5* (v[2:il+1,1:jlm]+v[2:il+1,2:jlm+1]) - vavg
-    uxw       = uavg - 0.5* (u[0:il-1,1:jlm]+u[0:il-1,2:jlm+1])
-    vxw       = vavg - 0.5* (v[0:il-1,1:jlm]+v[0:il-1,2:jlm+1])
+    xyw       = 0.5* (x[1:il,3:jlm+2,0]- x[1:il,1:jlm,0])
+    xye       = 0.5* (x[2:il+1,3:jlm+2,0]- x[2:il+1,1:jlm,0])
+    yyw       = 0.5* (x[1:il,3:jlm+2,1]- x[1:il,1:jlm,1])
+    yye       = 0.5* (x[2:il+1,3:jlm+2,1]- x[2:il+1,1:jlm,1])
+    volawi    = 2.0/(vola[2:il+1,2:jlm+1]+ vola[1:il,2:jlm+1])
+    volaei    = 2.0/(vola[2:il+1,2:jlm+1]+ vola[3:il+2,2:jlm+1])
+    uxe       = 0.5* (u[3:il+2,2:jlm+1]+u[3:il+2,3:jlm+2]) - uavg
+    vxe       = 0.5* (v[3:il+2,2:jlm+1]+v[3:il+2,3:jlm+2]) - vavg
+    uxw       = uavg - 0.5* (u[1:il,2:jlm+1]+u[1:il,3:jlm+2])
+    vxw       = vavg - 0.5* (v[1:il,2:jlm+1]+v[1:il,3:jlm+2])
     vor2      = 0.5* (xye* volaei* uxe+ xyw* volawi* uxw)
     vor3      = 0.5* (yye* volaei* vxe+ yyw* volawi* vxw)
     vort      = vor1- vor2- vor3
 
-    vor[1:il,1:jlm]  = abs(vort)
+    vor[2:il+1,2:jlm+1]  = abs(vort)
     utotal    = uavg* uavg+ vavg* vavg
-    utot[1:il,1:jlm] = np.sqrt(utotal)
+    utot[2:il+1,2:jlm+1] = np.sqrt(utotal)
 
     '''
     Determine transition index
@@ -168,20 +176,20 @@ def turbulent_viscosity(model, ws, state):
     itr1      = 0
     itr2      = 0
     j         = 1
-    for i in range(0,il):
-        if (x[i,j,1] <= xtran):
+    for i in range(1,il+1):
+        if (x[i,j,0] <= xtran):
             itr1      = i - 1
             break 
 
     itr1p     = itr1 + 1
-    for i in range(itr1p-1,il):
-        if (x[i,j,1] >= xtran):
+    for i in range(itr1p,il+1):
+        if (x[i,j,0] >= xtran):
             itr2      = i
             break
 
-    for i in range(1,il):
-        avor   = vor[i,0:jlm]
-        utot1  = utot[i,0:jlm]
+    for i in range(PAD,nx+PAD):
+        avor[1:jlm+1]   = vor[i,1:jlm+1]
+        utot1[1:jlm+1]  = utot[i,1:jlm+1]
 
         jmaxv     = np.argmax(avor) 
         if (jmaxv == 0): 
@@ -192,61 +200,60 @@ def turbulent_viscosity(model, ws, state):
 
         avorm[i]  = avor[jmaxv]
         utmin[i]  = utot1[jminut]
-        utmax[i]  = max(utot1[jmaxut],1.e-3)
+        utmax[i]  = maximum(utot1[jmaxut],1.e-3)
         utotm[i]  = utmax[i]
-        yscal[i]  = 1000000.
 
     if (modbl == 1):
         tur1    = 1.0
-        tur2    = 0.
-        tur3    = 0.
+        tur2    = 0.0
+        tur3    = 0.0
     elif (modbl == 2):
-        tur1    = 0.
+        tur1    = 0.0
         tur2    = 1.0
-        tur3    = 0.
+        tur3    = 0.0
     else:
-        tur1    = 0.
-        tur2    = 0.
+        tur1    = 0.0
+        tur2    = 0.0
         tur3    = 1.0
 
-    xxa       = x[itlp-1:itu,0,0]- x[itlp-1:itu-1,0,0]
-    yxa       = x[itlp-1:itu,0,1]- x[itlp-1:itu-1,0,1]
-    volai     = 1.0/vola[itlp-1:itu,0]
-    uy        = 2.0* u[itlp-1:itu,1]
-    amub      = .5* (amu[itlp-1:itu,0]+ amu[itlp-1:itu,1])
-    tauw[itlp-1:itu]   = amub* (xxa* uy)* volai
-    avor1     = vor[itlp-1:itu,0]
+    xxa       = x[itlp:itu,0,0]- x[itlp:itu-1,0,0]
+    yxa       = x[itlp:itu,0,1]- x[itlp:itu-1,0,1]
+    volai     = 1.0/vola[itlp:itu,1]
+    uy        = 2.0* u[itlp:itu,1]
+    amub      = 0.5* (amu[itlp:itu,1]+ amu[itlp:itu,2])
+    tauw[itlp:itu]   = amub* (xxa* uy)* volai
+    avor1     = vor[itlp:itu,1]
     avora     = avor1
-    avorb     = 0.5* (avor1+ avorm[itlp-1:itu])
-    avorc     = avorm[itlp-1:itu]
+    avorb     = 0.5* (avor1+ avorm[itlp:itu])
+    avorc     = avorm[itlp:itu]
     avor2     = tur1*avora + tur2*avorb + tur3*avorc
-    yscal[itlp-1:itu]  = np.sqrt(rey* sgrmi* amub* avor2* w[itlp-1:itu,1,0])/(26.*amub)
+    yscal[itlp:itu]  = np.sqrt(rey* sgrmi* amub* avor2* w[itlp:itu,2,0])/(26.0*amub)
     '''
     Compute normal distance ylen[i,j] and function 'yvor'
     (yvor = y* vorticity)
     '''
-    ylen[1:il,1] = 0.0
-    xc2       = .50* (x[1:il,1:jlm,0]+ x[0:il-1,1:jlm,0]-x[1:il,0:jlm-1,0]- x[0:il-1,0:jlm-1,0])
-    yc2       = .50* (x[1:il,1:jlm,1]+ x[0:il-1,1:jlm,1]-x[1:il,0:jlm-1,1]- x[0:il-1,0:jlm-1,1])
+    ylen[PAD:nx+PAD,1] = 0.0
+    xc2       = 0.5* (x[PAD:nx+PAD,PAD:ny+PAD,0]+ x[1:nx+1,PAD:ny+PAD,0]-x[PAD:nx+PAD,1:ny+1,0]- x[1:nx+1,1:ny+1,0])
+    yc2       = 0.5* (x[PAD:nx+PAD,PAD:ny+PAD,1]+ x[1:nx+1,PAD:ny+PAD,1]-x[PAD:nx+PAD,1:ny+1,1]- x[1:nx+1,1:ny+1,1])
 
     scalf  = np.sqrt(np.square(xc2) + np.square(yc2))
-    ylen[1:il,1:jlm] = ylen[1:il,0:jlm-1]+ scalf
+    ylen[PAD:nx+PAD,PAD:ny+PAD] = ylen[PAD:nx+PAD,1:ny+1]+ scalf
 
-    for i in range(1,il):
-        ylen1     = 0.5* ylen[i,1]
-        for j in range(0,int(np.floor(jstop))):
+    for i in range(PAD,nx+PAD):
+        ylen1     = 0.5* ylen[i,2]
+        for j in range(1,jstop+1):
             y1        = yscal[i]* ylen[i,j]
             damp      = 1.0- np.exp(-y1)
             yvor[j]   = ylen[i,j]* vor[i,j]* damp
 
         jmaxyv    = np.argmax(yvor)
-        jmaxyv    = max(jmaxyv,2)
+        jmaxyv    = maximum(jmaxyv,2)
         jedge[i]  = jmaxyv
 
-        yvorm[i]  = max(yvor[jmaxyv],1.e-6)
-        ylenm[i]  = max(ylen[i,jmaxyv],ylen1)
+        yvorm[i]  = maximum(yvor[jmaxyv],1.0e-6)
+        ylenm[i]  = maximum(ylen[i,jmaxyv],ylen1)
 
-        if (jedge[i] < int(np.floor(jstop))):
+        if (jedge[i] < jstop):
             ylenm1  = ylenm[i]
 
         if (ncyc>=10 or restarr==1.0):
@@ -267,18 +274,18 @@ def turbulent_viscosity(model, ws, state):
     '''
     Compute outer eddy viscosity
     '''
-    for i in range(1,il): 
+    for i in range(PAD,nx+PAD): 
         udiff     = abs(utmax[i]- utmin[i])
         udiff1    = cwk1* udiff
-        for j in range(1,int(np.floor(jstop))): 
+        for j in range(2,jstop+1): 
             ravg[j]   = 0.5* (w[i,j,0]+ w[i,j+1,0])
             coeff     = 0.0168* ccp
             fwake1    = coeff* yvorm[i]* ylenm[i]
             coeff2    = coeff* cwk1* cwk1
             fwake2    = coeff2* ylenm[i]* udiff* udiff/yvorm[i]
-            fwake     = min(fwake1,fwake2)
+            fwake     = minimum(fwake1,fwake2)
             fkleb0    = ckleb* ylen[i,j]/ylenm[i]
-            fkleb1    = min(fkleb0,1.e5)
+            fkleb1    = minimum(fkleb0,1.0e5)
             fkleb[j]  = 1.0/(1.0+ 5.5* fkleb1**6)
             amuto[j]  = rey* sgrmi* ravg[j]* fwake* fkleb[j]
             amuto[j]  = abs(amuto[j])
@@ -287,15 +294,14 @@ def turbulent_viscosity(model, ws, state):
         '''
         Compute inner eddy viscosity
         '''
-        j=int(np.floor(jstop))
-        y1        = yscal[i]* ylen[i,1:j]
+        y1        = yscal[i]* ylen[i,2:jstop+1]
         damp      = 1.0- np.exp(-y1)
         tscali    = 0.4* ylen[i,1:j]* damp
-        amuti1    = tscali* tscali* vor[i,1:j]
-        amuti[1:j]   = rey* sgrmi* ravg[1:j]* amuti1
-        amuti[1:j]   = abs(amuti[1:j])
+        amuti1    = tscali* tscali* vor[i,2:jstop+1]
+        amuti[2:jstop+1]   = rey* sgrmi* ravg[2:jstop+1]* amuti1
+        amuti[2:jstop+1]   = abs(amuti[2:jstop+1])
         amuti[1]  = 0.0
-        if (i<=itl or i>itu):
+        if (i<itl or i>=itu):
             amuti[1] = amuti[2]
 
         '''
@@ -305,46 +311,44 @@ def turbulent_viscosity(model, ws, state):
         ivect     = 1
         if (ivect == 0): 
             icross    = 0
-            amut[i,0] = amuti[0]
-            for j in range(1,int(np.floor(jstop))): 
+            amut[i,1] = amuti[1]
+            for j in range(2,jstop+1): 
                 if (amuti[j]<=amuto[j] and icross==0): 
                     amut[i,j] = amuti[j]
                 else:
                     icross    = 1
                     amut[i,j] = amuto[j]
         else: 
-            amut[i,0] = amuti[0]
-            ystop     = int(np.floor(jstop))
-            for j in range(0,int(np.floor(jstop))): 
+            amut[i,1] = amuti[1]
+            ystop     = jstop
+            for j in range(1,jstop+1): 
                 amudif    = amuti[j]- amuto[j]
                 if (amudif >= 0): 
-                    fcros[j] = j
+                    fcros[j] = (float)(j)
                 else: 
-                    fcros[j] = 1000
+                    fcros[j] = 1000.0
+                    
             jcros    = np.argmin(fcros)
             if (jcros == 1):
                 jcros = 2
             jcrosm   = jcros- 1
-            amut[i,0:jcrosm] = amuti[0:jcrosm]
-            j = int(np.floor(jstop))
-            amut[i,jcros-1:j] = amuto[jcros-1:j]
+            
+            amut[i,1:jcrosm+1] = amuti[1:jcrosm+1]
+            amut[i,jcros:jstop+1] = amuto[jcros:jstop+1]
         '''
         Compute turbulent viscosity at cell center
         '''
-        j=int(np.floor(jstop))
-        amutc     = 0.5* (amut[i,1:j]+ amut[i,0:j-1])
-        amu[i,1:j]  = amutc
-        amut[i,1:j] = amu[i,1:j]
+        amutc     = 0.5* (amut[i,2:jstop+1]+ amut[i,1:jstop])
+        amu[i,2:jstop+1]  = amutc
+        amut[i,2:jstop+1] = amu[i,2:jstop+1]
 
         if (i>itr1 and i<=itr2):
-            amut[i,1:j] = 0.
+            amut[i,2:jstop+1] = 0.0
 
     '''
     Copy amut to rev
     '''
-    scale     = 1.
-    rev[0:ie,0:je]  = scale*amut[0:ie,0:je]
+    scale     = 1.0
+    rev[1:ie+1,1:je+1]  = scale*amut[1:ie+1,1:je+1]
 
     return
-
-

bin/model_funcs/BoundaryThickness.py
@@ -18,46 +18,50 @@ def boundary_thickness(model, ws, state, ynot, dsti):
     x = ws.get_field('x')
     xc = mget('xc')
 
-    # defining local variables:
+    # set geometry parameters
+    PAD = model.padding
     [nx, ny] = ws.field_size()
-    il = nx
+    [nxp, nyp] = [PAD+nx+PAD, PAD+ny+PAD]
+    [il, jl] = [nx+1, ny+1]
+    [ie, je] = [nx+2, ny+2]
+    [ib, jb] = [nx+3, ny+3]
 
-    qs = np.ones(nx)
-    ut = np.ones(nx)
-    dn = np.ones(nx)
-    ssmax = np.ones(nx)
+    qs = np.ones(nxp)
+    ut = np.ones(nxp)
+    dn = np.ones(nxp)
+    ssmax = np.ones(nxp)
     
     js = 0.75*(ny - 4)
-    js = int(np.floor(js))-1
+    js = int(np.floor(js))
 
-    for i in range(il):
-        qs[0]     = 0.
-        ut[0]     = 0.
+    for i in range(PAD,nx+PAD):
+        qs[1]     = 0.0
+        ut[1]     = 0.0
         j         = js
-        xy        = .5*(x[i,j,0]  -x[i,j-1,0]+x[i-1,j,0]  -x[i-1,j-1,0])
-        yy        = .5*(x[i,j,1]  -x[i,j-1,1]+x[i-1,j,1]  -x[i-1,j-1,1])
+        xy        = 0.5*(x[i,j,0]  -x[i,j-1,0]+x[i-1,j,0]  -x[i-1,j-1,0])
+        yy        = 0.5*(x[i,j,1]  -x[i,j-1,1]+x[i-1,j,1]  -x[i-1,j-1,1])
         qs[j]     = (yy*w[i,j,1]  -xy*w[i,j,2])/(w[i,j,0])
         si        = np.copysign(1,qs[js])
 
-        for j in range(1,js):
-            xy        = .5*(x[i,j,0]  -x[i,j-1,0]+x[i-1,j,0]  -x[i-1,j-1,0])
-            yy        = .5*(x[i,j,1]  -x[i,j-1,1]+x[i-1,j,1]  -x[i-1,j-1,1])
-            dsi       = 1./np.sqrt(xy**2  +yy**2)
+        for j in range(2,js+1):
+            xy        = 0.5*(x[i,j,0]  -x[i,j-1,0]+x[i-1,j,0]  -x[i-1,j-1,0])
+            yy        = 0.5*(x[i,j,1]  -x[i,j-1,1]+x[i-1,j,1]  -x[i-1,j-1,1])
+            dsi       = 1.0/np.sqrt(xy**2  +yy**2)
             qs[j]     = si*(yy*w[i,j,1]  -xy*w[i,j,2])
-            dn[j]     = 1./dsi
+            dn[j]     = 1.0/dsi
             ut[j]     = qs[j]*dsi
 
-        dsti[i]   = 0.
-        ynot[i]   = 0.
-        ssmax[i]  = 0.
-        cdu       = .98
+        dsti[i]   = 0.0
+        ynot[i]   = 0.0
+        ssmax[i]  = 0.0
+        cdu       = 0.98
         jmax      = np.argmax(ut)
-        fx        = .6*ut[jmax]
+        fx        = 0.6*ut[jmax]
         lend      = 2
         lbig      = 2
         locke     = False
 
-        for  j in range(1,js):
+        for  j in range(3,js+1):
             if ( ut[j-1] < 0 and ut[j] >= 0):
                 lbig = j
 
@@ -67,26 +71,26 @@ def boundary_thickness(model, ws, state, ynot, dsti):
                     lend      = j
 
         uinf      = 1./ut[lend]
-        for j in range(lbig-1,lend):
+        for j in range(lbig,lend+1):
             dsti [i]  = dsti[i] + (ut[lend]*dn[j] - qs[j])
             ra        = w[i,lend,0]/w[i,j,0]
             ssmax[i]  = ssmax[i] + ra*ut[j]*uinf*(dn[j]-qs[j]*uinf)
 
         dsti[i]   = dsti[i]*uinf
         
-        dsti[i]  = max(dsti[i],1.e-6)
-        ra        = w[i,lend,0]/w[i,1,0]
+        dsti[i]  = max(dsti[i],1.0e-6)
+        ra        = w[i,lend,0]/w[i,2,0]
         ssmax[i]  = max(ssmax[i],ra*qs[1]*uinf)
         fc        = .95*ut[lend]
 
         jse       = lend
-        for j in range(1,jse):
+        for j in range(2,jse+1):
             lend      = j  -1
             if (ut[j] > fc):
                 break
 
-        xbi       = .5*(x[i,0,0]+x[i-1,0,0])
-        ybi       = .5*(x[i,0,1]+x[i-1,0,1])
+        xbi       = 0.5*(x[i,1,0]+x[i-1,1,0])
+        ybi       = 0.5*(x[i,1,1]+x[i-1,1,1])
         ycorr     = np.sqrt((xc[i,lend,0] - xbi)**2+(xc[i,lend,1]-ybi)**2)
         ynot[i]   = 1.5*(ycorr  +dn[lend]*(fc  -ut[lend])/(ut[lend+1]  -ut[lend]))
     return

bin/model_funcs/Viscosity.py
@@ -10,6 +10,7 @@ sys.path.append('../RANS/bin')
 import time
 
 import numpy as np
+from bin.Field import pos_diff
 from bin.model_funcs.BaldwinLomax import turbulent_viscosity
 from bin.model_funcs.BoundaryThickness import boundary_thickness
 
@@ -45,18 +46,18 @@ def compute_viscosity(model, ws, state):
     rev = ev
 
     # set geometry parameters
-    pad = model.padding
+    PAD = model.padding
     [nx, ny] = ws.field_size()
-    [nxp, nyp] = [pad+nx+pad, pad+ny+pad]
-    [il, jl] = [nx, ny]
-    [ie, je] = [nx+1, ny+1]
-    [ib, jb] = [nx+2, ny+2]
+    [nxp, nyp] = [PAD+nx+PAD, PAD+ny+PAD]
+    [il, jl] = [nx+1, ny+1]
+    [ie, je] = [nx+2, ny+2]
+    [ib, jb] = [nx+3, ny+3]
 
     dims = ws.get_dims()
-    itl = dims['itl'] + pad
-    itu = dims['itu'] + pad
-    ile = int(ie/2) + pad
-    chord = x[itl,pad,1] - x[ile,pad,1] # Not sure if correct FIX!!
+    itl = dims['itl'] + PAD
+    itu = dims['itu'] + PAD
+    ile = int(ie/2) + PAD
+    chord = geom['chord']
     
     geom = ws.get_geometry()
     scal = geom['scal']
@@ -71,8 +72,8 @@ def compute_viscosity(model, ws, state):
     rmu0 = model.params['mu0']
     
     # boundary layer parameters
-    ynot = np.ones(nx)
-    dsti = np.ones(nx)
+    ynot = np.ones(nxp)
+    dsti = np.ones(nxp)
 
     
     # select model
@@ -97,8 +98,8 @@ def compute_viscosity(model, ws, state):
     visc_const_C2 = 110.3
 
     # compute the molecular viscosity
-    tt       = p[0:ie,0:je]/w[0:ie,0:je,0]*t0
-    rlv[0:ie,0:je] = visc_const_C1*tt*np.sqrt(tt)/((tt+visc_const_C2)*rmu0)
+    tt       = p[1:ie+1,1:je+1]/w[1:ie+1,1:je+1,0]*t0
+    rlv[1:ie+1,1:je+1] = visc_const_C1*tt*np.sqrt(tt)/((tt+visc_const_C2)*rmu0)
 
 
     # for laminar flows we are done.
@@ -108,61 +109,61 @@ def compute_viscosity(model, ws, state):
         return
 
     aturb     = 1.
-    #if (ncyc > 25): commented out bc don't know how to implement
+    #if (ncyc > 25): commented out bc don't know how to implement FIX!!!
     #    aturb = .5
     if (kturb == 1): 
-        rev0[0:ie,0:je] = rev[0:ie,0:je]
+        rev0[1:ie+1,1:je+1] = rev[1:ie+1,1:je+1]
 
         '''
         If running laminar flows, calculation is more simple.
         Call either the Baldwin Lomax Model or run the RNG algebraic model.
         '''
-        turbulent_viscosity(model, ws, w)
+        turbulent_viscosity(model, ws, w) # CHECK THIS FILE FIX!!!
 
 
-        rev[0:ie,0:je] = aturb*rev[0:ie,0:je]  +(1.  -aturb)*rev0[0:ie,0:je]
+        rev[1:ie+1,1:je+1] = aturb*rev[1:ie+1,1:je+1]  +(1.0  -aturb)*rev0[1:ie+1,1:je+1]
     else:
         '''
         Otherwise, start the rng algebraic model.
         '''
-        u[0:ie,0:je]   = w[0:ie,0:je,1]/w[0:ie,0:je,0]
-        v[0:ie,0:je]   = w[0:ie,0:je,2]/w[0:ie,0:je,0]
+        u[1:ie+1,1:je+1]   = w[1:ie+1,1:je+1,1]/w[1:ie+1,1:je+1,0]
+        v[1:ie+1,1:je+1]   = w[1:ie+1,1:je+1,2]/w[1:ie+1,1:je+1,0]
 
         u[itl:itu,0]   = -u[itl:itu,1]
         v[itl:itu,0]   = -v[itl:itu,1]
 
-        dx13      = xc[0:il,0:jl,0]   - xc[1:il+1,1:jl+1,0]
-        dy13      = xc[0:il,0:jl,1]   - xc[1:il+1,1:jl+1,1]
-        dx24      = xc[1:il+1,0:jl,0] - xc[0:il,1:jl+1,0]
-        dy24      = xc[1:il+1,0:jl,1] - xc[0:il,1:jl+1,1]
-        du13      = u[0:il,0:jl] - u[1:il+1,1:jl+1]
-        dv13      = v[0:il,0:jl] - v[1:il+1,1:jl+1]
-        du24      = u[1:il+1,0:jl] - u[0:il,1:jl+1]
-        dv24      = v[1:il+1,0:jl] - v[0:il,1:jl+1]
-        dsij      = 1./(dx13*dy24 - dx24*dy13)
+        dx13      = xc[1:il+1,1:jl+1,0]   - xc[2:il+2,2:jl+2,0]
+        dy13      = xc[1:il+1,1:jl+1,1]   - xc[2:il+2,2:jl+2,1]
+        dx24      = xc[2:il+2,1:jl+1,0] - xc[1:il+1,2:jl+2,0]
+        dy24      = xc[2:il+2,1:jl+1,1] - xc[1:il+1,2:jl+2,1]
+        du13      = u[1:il+1,1:jl+1] - u[2:il+2,2:jl+2]
+        dv13      = v[1:il+1,1:jl+1] - v[2:il+2,2:jl+2]
+        du24      = u[2:il+2,1:jl+1] - u[1:il+1,2:jl+2]
+        dv24      = v[2:il+2,1:jl+1] - v[1:il+1,2:jl+2]
+        dsij      = 1.0/(dx13*dy24 - dx24*dy13)
         dvdx      =  dsij * (dv13*dy24 - dv24*dy13)
         dudy      = -dsij * (du13*dx24 - du24*dx13)
         dudx      =  dsij * (du13*dy24 - du24*dy13)
         dvdy      = -dsij * (dv13*dx24 - dv24*dx13)
-        astr[0:il,0:jl] = (dudy+dvdx)**2. +2.*(dudx**2  +dvdy**2  -((dudx+dvdy)**2)/3.)
+        astr[1:il+1,1:jl+1] = (dudy+dvdx)**2 +2.0*(dudx**2  +dvdy**2  -((dudx+dvdy)**2)/3.0)
 
     '''
     Calculates the boundary layer thickness.
     '''
-    boundary_thickness(model, ws, state, ynot, dsti) 
+    boundary_thickness(model, ws, state, ynot, dsti)
 
 
 
-    for j in range(jl):
-        for i in range(il):
-            xbi       = .5*(x[i-1,0,0]  +x[i,0,0])
-            ybi       = .5*(x[i-1,0,1]  +x[i,0,1])
-            astra     = .25*(astr[i-1,j-1]  +astr[i-1,j]+astr[i,j-1]    +astr[i,j])
-            if (i>=itl) and (i<=itu+1):
-                a3        = 1./(.225*abs(ynot[i]))
+    for j in range(PAD,nx+PAD):
+        for i in range(PAD,ny+PAD):
+            xbi       = 0.5*(x[i-PAD,0,0]  +x[i+1-PAD,0,0])
+            ybi       = 0.5*(x[i-PAD,0,1]  +x[i+1-PAD,0,1])
+            astra     = 0.25*(astr[i-1,j-1]  +astr[i-1,j]+astr[i,j-1]    +astr[i,j])
+            if (i>=itl-1) and (i<=itu):
+                a3        = 1.0/(0.225*abs(ynot[i]))
                 ysci      = np.sqrt((xc[i,j,0]  -xbi)**2  +(xc[i,j,1]  -ybi)**2)
                 ysc       = w[i,2,0]/(ysci*w[i,j,0])
-                csc       = 1./(ysc+a3)**2
+                csc       = 1.0/(ysc+a3)**2
             else:
                 csc       = (cwk*ynot[i])**2
 
@@ -177,55 +178,54 @@ def compute_viscosity(model, ws, state):
             Solves for the eddy viscosity
             '''
             
-            if (max(rnut0*a1-a2,0) == 0.):
+            if (pos_diff(rnut0*a1,a2) == 0.):
                 rev[i,j]  = 0.
                 continue 
             else:
                 rnut      = np.sqrt(a1)
             
-            k      = 0
             fac    = a2 - 1.
 
-            while k<201: 
-                den    = 1./(4.*rnut*rnut*rnut + fac)
+            MAX_ITER=200
+            for k in range(MAX_ITER):
+                den    = 1.0/(4.0*rnut**3 + fac)
                 rnut1  = rnut - (rnut**4+rnut*fac  -rnut0*rnut0*a1)*den
                 
-                if (abs((rnut1  -rnut))<=1.e-3):
-                    rev[i,j] = w[i,j,1]*max(rnut1-rnul,0)
+                if (abs((rnut1  -rnut))<=1.0e-3):
+                    rev[i,j] = w[i,j,0]*pos_diff(rnut1,rnul)
                     break 
                 else:
-                    k      = k  +1
-                    if (k>199):
-                        print([' iteration not converged ',i,j])
-                        print([' rnut = ',rnut,' rnut1 =',rnut1])
-
-                        rev[i,j]  = w[i,j,0]*max(rnut1-rnul,0)
+                    if (k==MAX_ITER-1):
+                        print(" iteration not converged ("+str(i)+", "+str(j)+")")
+                        print(" rnut = "+str(rnut)+" rnut1 = "+str(rnut1))
+                        
+                        rev[i,j]  = w[i,j,0]*pos_diff(rnut1,rnul)
                         break 
 
                     rnut   = rnut1
 
     #     adjust the near wake
-    ii        = ie
-    for i in range(1,itl+1):
+    ii        = il+1
+    for i in range(PAD,itl+1):
         ii        = ii  -1
-        for j in range(1,jl):
-            pex       = -(xc[i,1,0]  -xc[itl+1,1,0])/(20.*dsti[itl+1])
-            rev[i,j]  = rev[i,j]  +(rev[itl+1,j]  -rev[i,j])*np.exp(pex)
-            pex       = -(xc[ii,1,0]  -xc[itu,1,0])/(20.*dsti[itu])
-            rev[ii,j] = rev[ii,j]  +(rev[itu,j]  -rev[ii,j])*np.exp(pex)
+        for j in range(PAD,ny+PAD):
+            pex       = -(xc[i,2,0]  -xc[itl,2,0])/(20.0*dsti[itl])
+            rev[i,j]  = rev[i,j]  +(rev[itl,j]  -rev[i,j])*np.exp(pex)
+            pex       = -(xc[ii,2,0]  -xc[itu-1,2,0])/(20.0*dsti[itu-1])
+            rev[ii,j] = rev[ii,j]  +(rev[itu-1,j]  -rev[ii,j])*np.exp(pex)
 
-    ii        = ib  -i
-            
-    rev[1:il,je] = rev[1:il,jl]
-    rev[1:il,0]  = rev[ii,1]
+    for i in range(PAD,nx+PAD):
+        ii           = ib  -i 
+        rev[i,je] = rev[i,jl]
+        rev[1,1]  = rev[ii,2]
 
     for i in range(itl,itu):
-            if (xc[i,1,0] <= xtran):
-                for j in range(0,jl):
+            if (xc[i,2,0] <= xtran):
+                for j in range(1,jl+1):
                     rev[i,j]  = 0
             rev[i,1]  = -rev[i,2]
 
-    rev[0,0:je]  = rev[1,0:je]
-    rev[ie,0:je] = rev[il,0:je]
+    rev[1 ,1:je+1] = rev[2 ,1:je+1]
+    rev[ie,1:je+1] = rev[il,1:je+1]
 
     return